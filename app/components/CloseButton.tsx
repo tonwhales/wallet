d5ee5b1733784ae3b7d431cbcbef98bfad3443c7
vzhovnitsky <vzhovnitsky@gmail.com>
2022-01-20 17:00:47 +0300

    fix: fixed onPress style

--- app/components/CloseButton.tsx
+++ app/components/CloseButton.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import { Pressable, Image, StyleProp, ViewStyle } from "react-native";
 
 export const CloseButton = React.memo((props: { onPress?: () => void, style?: StyleProp<ViewStyle> }) => {
-    const [closePressedIn, setClosePressedIn] = React.useState(false);
 
     return (
         <Pressable
-            onPressIn={() => setClosePressedIn(true)}
-            onPressOut={() => setClosePressedIn(false)}
-            style={[props.style, { opacity: closePressedIn ? 0.5 : 1 }]}
+            style={({ pressed }) => { return [props.style, { opacity: pressed ? 0.5 : 1 }] }}
             onPress={props.onPress}
         >
             <Image source={require('../../assets/ic_close.png')} />

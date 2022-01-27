0fc1c037bd5882f40ede7a49c1a20957e260737a
vzhovnitsky <vzhovnitsky@gmail.com>
2022-01-27 12:46:46 +0300

    wip: added checkboxes on before wallet backup fragment

--- app/fragments/onboarding/WalletCreatedFragment.tsx
+++ app/fragments/onboarding/WalletCreatedFragment.tsx
@@ -1,11 +1,11 @@
-import { ScrollView, Text, useWindowDimensions, View } from "react-native";
+import { Pressable, ScrollView, Text, useWindowDimensions, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { RoundButton } from "../../components/RoundButton";
 import { fragment } from "../../fragment";
 import { useTypedNavigation } from "../../utils/useTypedNavigation";
 import LottieView from 'lottie-react-native';
 import { useTranslation } from "react-i18next";
-import React from "react";
+import React, { useState } from "react";
 import { Ionicons } from '@expo/vector-icons';
 
 export const WalletCreatedFragment = fragment(() => {
@@ -13,6 +13,9 @@ export const WalletCreatedFragment = fragment(() => {
     const navigation = useTypedNavigation();
     const { t } = useTranslation();
     const { height } = useWindowDimensions();
+    const [loose, setLoose] = useState(false);
+    const [share, setShare] = useState(false);
+    const [responsibility, setResponsibility] = useState(false);
 
     return (
         <View style={{
@@ -28,13 +31,14 @@ export const WalletCreatedFragment = fragment(() => {
                 loop={true}
                 style={{ width: height * 0.15, height: height * 0.15, marginBottom: 8 }}
             />
-            <Text style={{ marginHorizontal: 8, fontSize: 30, fontWeight: '700' }}>
+            <Text style={{ marginHorizontal: 8, fontSize: 26, fontWeight: '700', textAlign: 'center' }}>
                 {t('Back up your wallet')}
             </Text>
             <Text style={{
                 marginTop: 11, fontSize: 16,
                 fontWeight: '400',
-                textAlign: 'center'
+                textAlign: 'center',
+                color: '#6D6D71'
             }}>
                 {t('You will be shown a secret recovery phrase on the next screen. The recovery phrase is the only key to your wallet. It will allow you to recover access to your wallet if your phone is lost or stolen.')}
             </Text>
@@ -50,61 +54,83 @@ export const WalletCreatedFragment = fragment(() => {
                     paddingHorizontal: 16,
                 }}
             >
-                <View style={{
-                    flexDirection: 'row',
-                    marginBottom: 14,
-                    marginTop: 15
-                }}>
+                <Pressable
+                    style={({ pressed }) => {
+                        return {
+                            flexDirection: 'row',
+                            marginBottom: 14, marginTop: 15,
+                            opacity: pressed ? 0.5 : 1
+                        }
+                    }}
+                    onPress={() => setLoose(!loose)}
+                >
                     <Ionicons
-                        name="checkmark-circle-outline"
+                        name={loose ? 'checkmark-circle' : 'ellipse-outline'}
                         size={20}
-                        color="black"
+                        color={loose ? '#42A3EB' : '#B6B6BF'}
                         style={{
                             marginRight: 11,
                             marginLeft: 1
                         }}
                     />
-                    <Text style={{ fontSize: 16, fontWeight: '400', flexShrink: 1 }}>
+                    <Text style={{ fontSize: 14, fontWeight: '400', flexShrink: 1 }}>
                         {t('If I lose recovery phrase, my funds will be lost forever.')}
                     </Text>
-                </View>
-                <View style={{
-                    flexDirection: 'row',
-                    width: '100%',
-                    marginBottom: 14
-                }}>
-
+                </Pressable>
+                <Pressable
+                    style={({ pressed }) => {
+                        return {
+                            flexDirection: 'row',
+                            width: '100%', marginBottom: 14,
+                            opacity: pressed ? 0.5 : 1
+                        }
+                    }}
+                    onPress={() => setShare(!share)}
+                >
                     <Ionicons
-                        name="checkmark-circle-outline"
+                        name={share ? 'checkmark-circle' : 'ellipse-outline'}
                         size={20}
-                        color="black"
+                        color={share ? '#42A3EB' : '#B6B6BF'}
                         style={{
                             marginRight: 11,
                             marginLeft: 1
                         }}
                     />
-                    <Text style={{ fontSize: 16, fontWeight: '400', flexShrink: 1 }}>
+                    <Text style={{ fontSize: 14, fontWeight: '400', flexShrink: 1 }}>
                         {t('If I expose or share my recovery phrase to anybody, my funds can be stolen.')}
                     </Text>
-                </View>
-                <View style={{ flexDirection: 'row', marginBottom: 16 }}>
+                </Pressable>
+                <Pressable
+                    style={({ pressed }) => {
+                        return {
+                            flexDirection: 'row', marginBottom: 16,
+                            opacity: pressed ? 0.5 : 1
+                        }
+                    }}
+                    onPress={() => { setResponsibility(!responsibility) }}
+                >
                     <Ionicons
-                        name="checkmark-circle-outline"
+                        name={responsibility ? 'checkmark-circle' : 'ellipse-outline'}
                         size={20}
-                        color="black"
+                        color={responsibility ? '#42A3EB' : '#B6B6BF'}
                         style={{
                             marginRight: 11,
                             marginLeft: 1
                         }}
                     />
-                    <Text style={{ fontSize: 16, fontWeight: '400', flexShrink: 1 }}>
+                    <Text style={{ fontSize: 14, fontWeight: '400', flexShrink: 1 }}>
                         {t('It is my full responsibility to keep my recovery phrase secure.')}
                     </Text>
-                </View>
+                </Pressable>
             </View>
             <View style={{ flexGrow: 1, marginBottom: 32 }} />
             <View style={{ height: 64, position: 'absolute', bottom: safeArea.bottom, left: 16, right: 16 }}>
-                <RoundButton title={t("Back up now")} onPress={() => navigation.navigate('WalletBackupInit')} />
+                <RoundButton
+                    display={(loose && share && responsibility) ? 'default' : 'secondary'}
+                    disabled={!(loose && share && responsibility)}
+                    title={t("Back up now")}
+                    onPress={() => navigation.navigate('WalletBackupInit')}
+                />
             </View>
         </View>
     );

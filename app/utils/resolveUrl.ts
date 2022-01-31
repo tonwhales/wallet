5b22e66693018406a2b40adab4847a81e533e1b8
Steve Korshakov <steve@korshakov.com>
2022-01-31 16:01:45 +0300

    feat: suppor test ton endpoint

--- app/utils/resolveUrl.ts
+++ app/utils/resolveUrl.ts
@@ -29,7 +29,7 @@ export function resolveUrl(src: string): {
         const url = new Url(src, true);
 
         // ton url
-        if (url.protocol.toLowerCase() === 'ton:' && url.host.toLowerCase() === 'transfer' && url.pathname.startsWith('/')) {
+        if ((url.protocol.toLowerCase() === 'ton:' || url.protocol.toLowerCase() === 'ton-test:') && url.host.toLowerCase() === 'transfer' && url.pathname.startsWith('/')) {
             let address = Address.parseFriendly(url.pathname.slice(1)).address;
             let comment: string | null = null;
             let amount: BN | null = null;

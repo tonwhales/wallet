76e69ed03425cab773de94f437974b7237e2ade3
Steve Korshakov <steve@korshakov.com>
2022-01-31 16:00:51 +0300

    feat: support test endpoints

--- app/utils/resolveUrl.ts
+++ app/utils/resolveUrl.ts
@@ -62,7 +62,7 @@ export function resolveUrl(src: string): {
 
         // HTTP(s) url
         if ((url.protocol.toLowerCase() === 'http:' || url.protocol.toLowerCase() === 'https:')
-            && (url.host.toLowerCase() === 'tonhub.com' || url.host.toLowerCase() === 'www.tonhub.com')
+            && (url.host.toLowerCase() === 'tonhub.com' || url.host.toLowerCase() === 'www.tonhub.com' || url.host.toLowerCase() === 'test.tonhub.com')
             && (url.pathname.toLowerCase().startsWith('/transfer/'))) {
             let address = Address.parseFriendly(url.pathname.slice('/transfer/'.length)).address;
             let comment: string | null = null;

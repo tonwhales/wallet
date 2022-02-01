ffcd7fd58a8a3eec213d6fa42019643662dfe478
Steve Korshakov <steve@korshakov.com>
2022-02-01 17:02:20 +0300

    fix: fix scanner activity indicator

--- app/fragments/utils/ScannerFragment.tsx
+++ app/fragments/utils/ScannerFragment.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Image, Pressable, Platform } from 'react-native';
+import { Text, View, StyleSheet, Image, Pressable, Platform, ActivityIndicator } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { fragment } from '../../fragment';
 import { StatusBar } from 'expo-status-bar';
@@ -123,7 +123,7 @@ export const ScannerFragment = fragment(() => {
         <View style={styles.container}>
             <StatusBar style='light' />
             {(!device || !isActive) && (
-                <LoadingIndicator simple />
+                <ActivityIndicator color={'white'} />
             )}
             {device && isActive && (
                 <Camera

1d2b5fa2ab4f26acaea291d83593213d8082aea5
Steve Korshakov <steve@korshakov.com>
2022-02-11 01:18:11 +0300

    feat: android deep linking

--- android/app/src/main/java/com/tonhub/wallet/MainApplication.java
+++ android/app/src/main/java/com/tonhub/wallet/MainApplication.java
@@ -61,7 +61,7 @@ public class MainApplication extends Application implements ReactApplication {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
 
-        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+        // initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
         ApplicationLifecycleDispatcher.onApplicationCreate(this);
     }
 
@@ -78,27 +78,27 @@ public class MainApplication extends Application implements ReactApplication {
      * @param context
      * @param reactInstanceManager
      */
-    private static void initializeFlipper(
-            Context context, ReactInstanceManager reactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-            try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-                Class<?> aClass = Class.forName("com.tonhub.wallet.ReactNativeFlipper");
-                aClass
-                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-                        .invoke(null, context, reactInstanceManager);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    private static void initializeFlipper(
+//            Context context, ReactInstanceManager reactInstanceManager) {
+//        if (BuildConfig.DEBUG) {
+//            try {
+//        /*
+//         We use reflection here to pick up the class that initializes Flipper,
+//        since Flipper library is not available in release mode
+//        */
+//                Class<?> aClass = Class.forName("com.tonhub.wallet.ReactNativeFlipper");
+//                aClass
+//                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+//                        .invoke(null, context, reactInstanceManager);
+//            } catch (ClassNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (NoSuchMethodException e) {
+//                e.printStackTrace();
+//            } catch (IllegalAccessException e) {
+//                e.printStackTrace();
+//            } catch (InvocationTargetException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }

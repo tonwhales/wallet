73cd60617ac4a947eeedc0290a2a299849c3627a
vzhovnitsky <vzhovnitsky@gmail.com>
2022-02-25 16:09:09 +0300

    fix: fixed 12hour time format to enUS

--- app/utils/dates.ts
+++ app/utils/dates.ts
@@ -1,6 +1,7 @@
 import { endOfDay, format, isThisYear, isToday, isYesterday, Locale } from "date-fns";
 import { t } from "../i18n/t";
 import { ru } from 'date-fns/locale';
+import { enUS } from 'date-fns/locale'
 import * as RNLocalize from 'react-native-localize';
 
 const is24Hour = RNLocalize.uses24HourClock();
@@ -23,7 +24,10 @@ export function formatDate(src: number, dateFormat?: string) {
 }
 
 export function formatTime(src: number) {
-    return format(src * 1000, is24Hour || locale === ru ? 'HH:mm' : 'hh:mm aa', { locale });
+    if (is24Hour) {
+        return format(src * 1000, 'HH:mm', { locale });
+    }
+    return format(src * 1000, 'hh:mm aa', { locale: enUS });
 }
 
 export function getDateKey(src: number) {

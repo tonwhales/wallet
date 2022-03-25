e50f0bbc68cce57a4ccfe8d76167a41181bc2764
vzhovnitsky <vzhovnitsky@gmail.com>
2022-03-25 17:28:40 +0300

    wip: added divider

--- app/components/Staking/StakingMemberComponent.tsx
+++ app/components/Staking/StakingMemberComponent.tsx
@@ -224,6 +224,7 @@ export const StakingMemberComponent = React.memo((props: {
                 )}
                 {props.member.withdraw.gtn(0) && (
                     <View style={{ flexDirection: 'column' }}>
+                        <View style={{ height: 1, alignSelf: 'stretch', backgroundColor: Theme.divider, marginVertical: 16 }} />
                         <View style={{ justifyContent: 'space-between', flexDirection: 'column', alignItems: 'flex-start' }}>
                             <Text style={{ fontSize: 14, color: Theme.textSecondary }}>
                                 {t('products.staking.withdrawStatus.ready')}

057ee564e6b4b82c55158f602b83fb65d9459532
vzhovnitsky <vzhovnitsky@gmail.com>
2022-04-18 20:27:12 +0300

    fix: countdown format

--- app/components/Countdown.tsx
+++ app/components/Countdown.tsx
@@ -7,23 +7,43 @@ import { t } from "../i18n/t";
 
 function getDuration(seconds: number) {
     let left = seconds;
-    const days = Math.floor(left / (24 * 60 * 60));
-    left = left - days * (24 * 60 * 60);
     const hours = Math.floor(left / (60 * 60));
     left = left - hours * 60 * 60
     const minutes = Math.floor(left / 60);
     left = left - minutes * 60;
-    let secs;
-
-    if (days === 0 && hours === 0) {
-        secs = left
-    }
 
     return {
-        days,
         hours,
         minutes,
-        seconds: secs
+        seconds: left
+    }
+}
+
+type DurationKey = 'xSeconds' | 'xMinutes' | 'xHours' | 'xDays';
+
+const formatDurationRuLocale: { [key in DurationKey]: string } = {
+    xSeconds: '{{count}}',
+    xMinutes: '{{count}}',
+    xHours: '{{count}}',
+    xDays: '{{count}}'
+}
+
+const formatDurationEnLocale: { [key in DurationKey]: string } = {
+    xSeconds: '{{count}}',
+    xMinutes: '{{count}}',
+    xHours: '{{count}}',
+    xDays: '{{count}}'
+}
+
+const shortRuLocale = {
+    formatDistance: (key: DurationKey, count: any) => {
+        return formatDurationRuLocale[key].replace('{{count}}', String(count || '').padStart(2, '0'));
+    }
+}
+
+const shortEnLocale = {
+    formatDistance: (key: DurationKey, count: number) => {
+        return formatDurationEnLocale[key].replace('{{count}}', String(count || '').padStart(2, '0'))
     }
 }
 
@@ -31,40 +51,30 @@ function format(duration: number) {
     if (duration <= 0) return t('common.soon');
     return t('common.in')
         + ' '
-        + formatDuration(getDuration(duration), { locale: t('lang') === 'ru' ? ru : undefined });
+        + formatDuration(
+            getDuration(duration),
+            { locale: t('lang') === 'ru' ? shortRuLocale : shortEnLocale, delimiter: ':', zero: true }
+        );
 }
 
 export const Countdown = React.memo(({ until, textStyle }: { until: number, textStyle?: StyleProp<TextStyle> }) => {
     const { t } = useTranslation()
-    const [text, setText] = useState(format(Math.floor(until - (Date.now() / 1000))));
+    const [text, setText] = useState(format(Math.floor((until || 0) - (Date.now() / 1000))));
 
     useEffect(() => {
         const timerId = setInterval(() => {
-            setText(format(Math.floor(until - (Date.now() / 1000))));
+            setText(format(Math.floor((until || 0) - (Date.now() / 1000))));
         }, 1000);
         return () => {
             clearInterval(timerId);
         };
     }, [until]);
 
-    if (!until) {
-        return (
-            <Text style={[{
-                color: '#8E979D',
-                fontSize: 13,
-            }, textStyle]}
-                ellipsizeMode="tail"
-                numberOfLines={1}
-            >
-                {t('common.soon')}
-            </Text>
-        );
-    }
-
     return (
         <Text style={[{
             color: '#8E979D',
             fontSize: 13,
+            fontVariant: ['tabular-nums']
         }, textStyle]}
             ellipsizeMode="tail"
             numberOfLines={1}

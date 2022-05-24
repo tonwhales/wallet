86489c4209041b2138350c6ec98006bce93b66aa
Steve Korshakov <steve@korshakov.com>
2022-05-24 20:00:03 +0400

    Revert "wip: working on bluetooth integration"
    This reverts commit f738d28ff182f3a7b3690eddd931b7af39d7f660.

--- app/Navigation.tsx
+++ app/Navigation.tsx
@@ -40,7 +40,6 @@ import { SignFragment } from './fragments/secure/SignFragment';
 import { TransferFragment } from './fragments/secure/TransferFragment';
 import { createEngine } from './engine/createEngine';
 import { useRecoilCallback } from 'recoil';
-import { DevBluetoothFragment } from './fragments/dev/DevBluetoothFragment';
 
 const Stack = createNativeStackNavigator();
 // const Stack = Platform.OS === 'ios' ? createNativeStackNavigator() : createStackNavigator();
@@ -134,12 +133,10 @@ const navigation = [
     modalScreen('Sign', SignFragment),
     modalScreen('Migration', MigrationFragment),
     lockedModalScreen('Scanner', ScannerFragment),
+    genericScreen('DeveloperTools', DeveloperToolsFragment),
     modalScreen('Buy', NeocryptoFragment),
     fullScreen('Staking', StakingFragment),
     modalScreen('StakingTransfer', StakingTransferFragment),
-
-    genericScreen('Dev', DeveloperToolsFragment),
-    genericScreen('DevBluetooth', DevBluetoothFragment),
 ];
 
 export const Navigation = React.memo(() => {

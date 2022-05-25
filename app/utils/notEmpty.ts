e7ccacb16daecf28506e2cdde1695f3ce06fddae
vzhovnitsky <vzhovnitsky@gmail.com>
2022-05-25 16:00:50 +0300

    cleanup: rm useless files

--- app/utils/notEmpty.ts
+++
@@ -1,5 +0,0 @@
-export function notEmpty<T>(value: T | null | undefined): value is T {
-    if (value === null || value === undefined) return false;
-    const testDummy: T = value;
-    return true;
-  }

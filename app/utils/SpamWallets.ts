a7c8dcc64e9154619a02e0e3be661d35ddb4e0a3
vzhovnitsky <vzhovnitsky@gmail.com>
2022-05-25 17:37:15 +0300

    add: added explorer links

--- app/utils/SpamWallets.ts
+++ app/utils/SpamWallets.ts
@@ -8,3 +8,9 @@ export const SpamWallets: string[] = [
     Address.parse('EQB-_cnYRcANzdr4TCumgIssPBU4-Jdp5EeA7KBNme8cKiWF').toFriendly(),
     Address.parse('EQCs5apjBPO1kKnkeoJk_NFsxG_V5jOnrcVKjmngInOJQ-0-').toFriendly(),
 ];
+// https://tonwhales.com/explorer/address/EQDUgUAIVeCCkf0sz_ZhX-w5b2YtmAVMlQuR0uX0A2Kg-jHX
+// https://tonwhales.com/explorer/address/EQA5XOBReFMefJ1rvSmHptS051lNw8DZb2enNnpXjZwHB2u3
+// https://tonwhales.com/explorer/address/EQAGX5xBZOxzydbRL-m3yVsjUN2dUy7Lwo8_o71un-1qB-lD
+// https://tonwhales.com/explorer/address/EQDs5J9ZvBCRoCgLXYevdMZregqLs2EM0QZEgxebsEAYUV-2
+// https://tonwhales.com/explorer/address/EQB-_cnYRcANzdr4TCumgIssPBU4-Jdp5EeA7KBNme8cKiWF
+// https://tonwhales.com/explorer/address/EQCs5apjBPO1kKnkeoJk_NFsxG_V5jOnrcVKjmngInOJQ-0-'

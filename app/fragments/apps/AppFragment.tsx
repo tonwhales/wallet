18d2ad5da01c18dd2e8c6c21633f071d2d3889f7
vzhovnitsky <vzhovnitsky@gmail.com>
2022-06-30 15:41:01 +0300

    Merge branch 'origin-master' into apps-more-menu
    # Conflicts:
    #	app/fragments/apps/AppFragment.tsx

--- app/fragments/apps/AppFragment.tsx
+++ app/fragments/apps/AppFragment.tsx
@@ -11,13 +11,11 @@ import { extractDomain } from '../../engine/utils/extractDomain';
 import { useEngine } from '../../engine/Engine';
 import { RoundButton } from '../../components/RoundButton';
 import { t } from '../../i18n/t';
-import MoreIcon from '../../../assets/ic_more.svg';
-import { MenuView } from '@react-native-menu/menu';
-import { generateAppLink } from '../../utils/generateAppLink';
+import { MixpanelEvent, trackEvent, useTrackEvent } from '../../analytics/mixpanel';
 
 export const AppFragment = fragment(() => {
     const engine = useEngine();
-    const url = (useRoute().params as any).url;
+    const url = (useRoute().params as any).url as string;
     const domain = extractDomain(url);
     const appData = engine.products.extensions.useAppData(url);
     const safeArea = useSafeAreaInsets();
@@ -27,6 +25,15 @@ export const AppFragment = fragment(() => {
     const dark = c.isDark();
     const fontColor = dark ? '#fff' : '#000';
     const key = engine.persistence.domainKeys.getValue(domain);
+    const start = React.useMemo(() => {
+        return Date.now();
+    }, []);
+    const close = React.useCallback(() => {
+        navigation.goBack();
+        trackEvent(MixpanelEvent.AppClose, { url, domain, duration: Date.now() - start });
+    }, []);
+    useTrackEvent(MixpanelEvent.AppOpen, { url, domain });
+
     if (!appData) {
         throw Error('No App Data');
     }
@@ -63,29 +70,8 @@ export const AppFragment = fragment(() => {
                 domainKey={key}
             />
 
-            <View style={{ flexDirection: 'row', height: 50 + safeArea.bottom, alignItems: 'center', justifyContent: 'center', paddingBottom: safeArea.bottom }}>
-                <RoundButton
-                    title={t('common.close')}
-                    display="secondary"
-                    size="normal"
-                    style={{ paddingHorizontal: 8 }}
-                    onPress={() => navigation.goBack()}
-                />
-                <MenuView
-                    style={{
-                        position: 'absolute',
-                        top: 8, right: 16,
-                        height: 32
-                    }}
-                    onPressAction={({ nativeEvent }) => {
-                        if (nativeEvent.event === 'share') onShare();
-                    }}
-                    actions={[
-                        { title: t('common.share'), id: 'share', image: Platform.OS === 'ios' ? 'square.and.arrow.up' : undefined },
-                    ]}
-                >
-                    <MoreIcon color={'black'} height={30} width={30} />
-                </MenuView>
+            <View style={{ height: 50 + safeArea.bottom, alignItems: 'center', justifyContent: 'center', paddingBottom: safeArea.bottom }}>
+                <RoundButton title={t('common.close')} display="secondary" size="normal" style={{ paddingHorizontal: 8 }} onPress={close} />
             </View>
         </View>
     );

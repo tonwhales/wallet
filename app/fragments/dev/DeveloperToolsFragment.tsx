3a847449db0cc01fed3ada68d005aa38b11fd966
vzhovnitsky <vzhovnitsky@gmail.com>
2022-07-25 14:15:12 +0300

    Merge branch 'origin-master' into plugins-fragment
    # Conflicts:
    #	app/Navigation.tsx
    #	app/fragments/dev/DeveloperToolsFragment.tsx

--- app/fragments/dev/DeveloperToolsFragment.tsx
+++ app/fragments/dev/DeveloperToolsFragment.tsx
@@ -1,34 +1,21 @@
 import * as React from 'react';
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import { ItemButton } from "../../components/ItemButton";
 import { Item } from '../../components/Item';
 import { AppConfig } from '../../AppConfig';
 import { useReboot } from '../../utils/RebootContext';
 import { fragment } from '../../fragment';
-import { storage, storagePersistence } from '../../storage/storage';
-import { getApplicationKey, loadKeyStorageRef, loadKeyStorageType } from '../../storage/secureStorage';
-import { getCurrentAddress } from '../../storage/appState';
-import BN from 'bn.js';
-import { Theme } from '../../Theme';
+import { storagePersistence } from '../../storage/storage';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useTypedNavigation } from '../../utils/useTypedNavigation';
+import { StatusBar } from 'expo-status-bar';
+import { AndroidToolbar } from '../../components/AndroidToolbar';
+import { useEngine } from '../../engine/Engine';
 
 export const DeveloperToolsFragment = fragment(() => {
     const navigation = useTypedNavigation();
+    const safeArea = useSafeAreaInsets();
     const reboot = useReboot();
-    let ref = loadKeyStorageRef();
-    let kind = loadKeyStorageType();
-    let [value, setValue] = React.useState('');
-    React.useEffect(() => {
-        (async () => {
-            try {
-                let key = await getApplicationKey();
-                setValue(key.toString('base64'));
-            } catch (e) {
-                console.warn(e);
-                setValue('error');
-            }
-        })();
-    }, []);
     const restart = React.useCallback(() => {
         // TODO: Implement
     }, [])
@@ -36,6 +23,11 @@ export const DeveloperToolsFragment = fragment(() => {
         storagePersistence.clearAll();
         reboot();
     }, []);
+
+    const engine = useEngine();
+    const counter = React.useMemo(() => engine.cloud.counter('counter.sample'), []);
+    const counterValue = counter.use().counter;
+
     // const isTestNet = useTestnet();
     // const switchNetwork = React.useCallback(() => {
     //     let state = (getAppState())!;
@@ -43,32 +35,46 @@ export const DeveloperToolsFragment = fragment(() => {
     //     reboot();
     // }, []);
     return (
-        <View style={{ backgroundColor: Theme.background, flexGrow: 1, flexBasis: 0, paddingHorizontal: 16, marginTop: 64 }}>
-            <View style={{
-                marginBottom: 16, marginTop: 17,
-                backgroundColor: "white",
-                borderRadius: 14,
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexShrink: 1,
-            }}>
-                <View style={{ marginHorizontal: 16, width: '100%' }}>
-                    <ItemButton leftIcon={require('../../../assets/ic_sign_out.png')} dangerZone title={'Clean cache and reset'} onPress={resetCache} />
-                </View>
-                <View style={{ marginHorizontal: 16, width: '100%' }}>
-                    <ItemButton leftIcon={require('../../../assets/ic_sign_out.png')} dangerZone title={"Restart app"} onPress={restart} />
-                </View>
-                <View style={{ marginHorizontal: 16, width: '100%' }}>
-                    <Item title={"Version"} hint={AppConfig.isTestnet ? 'Testnet' : 'Mainnet'} />
-                </View>
-                <View style={{ marginHorizontal: 16, width: '100%' }}>
-                    <Item title={"Storage Kind"} hint={kind} />
-                </View>
-                <View style={{ marginHorizontal: 16, width: '100%' }}>
-                    <Item title={"Storage Ref"} hint={ref} />
-                </View>
-                <View style={{ marginHorizontal: 16, width: '100%' }}>
-                    <Item title={"Storage Key"} hint={value} />
+        <View style={{
+            flex: 1,
+            paddingTop: Platform.OS === 'android' ? safeArea.top : undefined,
+        }}>
+            <StatusBar style={'dark'} />
+            <AndroidToolbar pageTitle={'Dev Tools'} />
+            <View style={{ backgroundColor: Theme.background, flexGrow: 1, flexBasis: 0, paddingHorizontal: 16, marginTop: 0 }}>
+                <View style={{
+                    marginBottom: 16, marginTop: 17,
+                    backgroundColor: "white",
+                    borderRadius: 14,
+                    overflow: 'hidden',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexShrink: 1,
+                }}>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <ItemButton leftIcon={require('../../../assets/ic_sign_out.png')} dangerZone title={'Clean cache and reset'} onPress={resetCache} />
+                    </View>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <ItemButton leftIcon={require('../../../assets/ic_sign_out.png')} dangerZone title={"Restart app"} onPress={restart} />
+                    </View>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <ItemButton title={"Storage Status"} onPress={() => navigation.navigate('DeveloperToolsStorage')} />
+                    </View>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <ItemButton title={"Test Whales"} onPress={() => navigation.navigate('Install', { url: AppConfig.isTestnet ? 'https://sandbox.tonwhales.com/tools/x' : 'https://tonwhales.com/tools/x' })} />
+                    </View>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <ItemButton title={"Test Scaleton"} onPress={() => navigation.navigate('Install', { url: AppConfig.isTestnet ? 'https://sandbox.scaleton.io' : 'https://scaleton.io' })} />
+                    </View>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <ItemButton title={"Test Gems"} onPress={() => navigation.navigate('Install', { url: AppConfig.isTestnet ? 'https://getgems.io' : 'https://getgems.io' })} />
+                    </View>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <ItemButton title={"Cloud Counter"} hint={counterValue.toString()} onPress={() => counter.update((src) => src.counter.increment())} />
+                    </View>
+                    <View style={{ marginHorizontal: 16, width: '100%' }}>
+                        <Item title={"Version"} hint={AppConfig.isTestnet ? 'Testnet' : 'Mainnet'} />
+                    </View>
                 </View>
                 {AppConfig.isTestnet && (
                     <View style={{ marginHorizontal: 16, width: '100%' }}>

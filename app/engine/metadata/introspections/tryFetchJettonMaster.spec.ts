89a93fac2af2e7e2dd03de301733c043fb613185
vzhovnitsky <vzhovnitsky@gmail.com>
2022-09-05 17:01:34 +0300

    fix: fixed onchain jetton masters content

--- app/engine/metadata/introspections/tryFetchJettonMaster.spec.ts
+++ app/engine/metadata/introspections/tryFetchJettonMaster.spec.ts
@@ -11,7 +11,16 @@ describe('tryFetchJettonMaster', () => {
         expect(res.owner.equals(Address.parse('EQCdp7fR_iHcxO8PGuVbV6DgleITPLPebeQmfZdExgxPFj3a'))).toBe(true);
         expect(res.content).not.toBeNull();
         expect(res.content!.type).toEqual('offchain');
-        expect(res.content!.link).toEqual('https://kotecoin.com/kote.json');
+        expect((res.content! as { type: 'offchain', link: string }).link).toEqual('https://kotecoin.com/kote.json');
+    });
+    
+    it('should fetch onchain wallet', async () => {
+        let res = (await tryFetchJettonMaster(client, 23312939, Address.parse('EQD8a5p-Imf9cU55wB66guXwNSGv8lSGg-zFregbVXY0V51L')))!;
+        expect(res.mintalbe).toBe(true);
+        expect(res.owner.equals(Address.parse('EQDerEPTIh0O8lBdjWc6aLaJs5HYqlfBN2Ruj1lJQH_6vcaZ'))).toBe(true);
+        expect(res.totalSupply.toString(10)).toEqual('1231333000000000');
+        expect(res.content).not.toBeNull();
+        expect(res.content!.type).toEqual('onchain');
     });
 
     it('should not fetch KOTE wallet for old block', async () => {

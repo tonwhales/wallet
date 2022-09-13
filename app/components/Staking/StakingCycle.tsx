ce6b395f943bfde5d866bb08ea760f5b3dea79c4
vzhovnitsky <vzhovnitsky@gmail.com>
2022-09-13 15:27:26 +0300

    wip: adding cooldown info

--- app/components/Staking/StakingCycle.tsx
+++ app/components/Staking/StakingCycle.tsx
@@ -1,9 +1,13 @@
-import React from "react"
+import { formatDuration } from "date-fns"
+import React, { useEffect, useMemo, useState } from "react"
 import { View, Text, StyleProp, ViewStyle } from "react-native"
+import { AppConfig } from "../../AppConfig"
 import { t } from "../../i18n/t"
 import { Theme } from "../../Theme"
-import { Countdown } from "../Countdown"
-import { StakingCycleProgress } from "./StakingCycleProgress"
+import { Countdown, getDuration, shortLocale } from "../Countdown"
+import { stakingCycle, StakingCycleProgress } from "./StakingCycleProgress"
+
+const cooldownPeriod = AppConfig.isTestnet ? 30 * 60 : 2 * 60 * 60;
 
 export const StakingCycle = React.memo((
     {
@@ -14,44 +18,113 @@ export const StakingCycle = React.memo((
         stakeUntil: number,
         style?: StyleProp<ViewStyle>,
         withdraw?: boolean
-    }) => {
+    }
+) => {
+    const [left, setLeft] = useState(Math.floor(stakeUntil - (Date.now() / 1000)));
+
+    const cooldownActive = useMemo(() => left > stakingCycle - cooldownPeriod, [left]);
+    const cooldownDuration = useMemo(() => {
+        const duration = left - (stakingCycle - cooldownPeriod);
+        if (duration <= 0) {
+            return '';
+        }
+        return formatDuration(getDuration(duration), { locale: shortLocale(t('lang') as 'ru' | 'en'), delimiter: ':', zero: true });
+    }, [left, cooldownActive]);
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            setLeft(Math.floor((stakeUntil) - (Date.now() / 1000)));
+        }, 1000);
+        return () => {
+            clearInterval(timerId);
+        };
+    }, [stakeUntil]);
+
     return (
         <View style={[{
-            backgroundColor: 'white',
+            backgroundColor: Theme.item,
             minHeight: 70,
             borderRadius: 14,
-            paddingTop: 15,
-            paddingBottom: 12,
             marginHorizontal: 16,
             overflow: 'hidden',
         }, style]}>
-            <StakingCycleProgress
-                stakeUntil={stakeUntil}
-            />
-            <View style={{ flex: 1, paddingHorizontal: 16 }}>
-                <View style={{ justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row' }}>
-                    <Text style={{
-                        color: Theme.textColor,
-                        fontWeight: '600',
-                        fontSize: 16
+            {!cooldownActive && (
+                <>
+                    <StakingCycleProgress left={left} />
+                    <View style={{
+                        flex: 1,
+                        paddingHorizontal: 16,
+                        paddingTop: 15,
+                        paddingBottom: 8
                     }}>
-                        {t('products.staking.nextCycle')}
-                    </Text>
-                    <Countdown until={stakeUntil} textStyle={{
-                        fontWeight: '400',
-                        color: Theme.textColor,
-                        fontSize: 16
-                    }} />
-                </View>
-                <Text style={{
-                    color: Theme.textSecondary,
-                    fontWeight: '400',
-                    fontSize: 13,
-                    marginTop: 8
-                }}>
-                    {withdraw ? t('products.staking.cycleNoteWithdraw') : t('products.staking.cycleNote')}
-                </Text>
-            </View>
+                        <View style={{ justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row' }}>
+                            <Text style={{
+                                color: Theme.textColor,
+                                fontWeight: '600',
+                                fontSize: 16
+                            }}>
+                                {t('products.staking.nextCycle')}
+                            </Text>
+                            <Countdown
+                                left={left}
+                                textStyle={{
+                                    fontWeight: '400',
+                                    color: Theme.textColor,
+                                    fontSize: 16
+                                }}
+                            />
+                        </View>
+                        <Text style={{
+                            color: Theme.textSecondary,
+                            fontWeight: '400',
+                            fontSize: 13,
+                        }}>
+                            {withdraw ? t('products.staking.cycleNoteWithdraw') : t('products.staking.cycleNote')}
+                        </Text>
+                    </View>
+                </>
+            )}
+            {cooldownActive && (
+                <>
+                    <StakingCycleProgress
+                        left={left - (stakingCycle - cooldownPeriod)}
+                        full={cooldownPeriod}
+                    />
+                    <View style={{
+                        flex: 1,
+                        paddingHorizontal: 16,
+                        paddingTop: 15,
+                        paddingBottom: 8
+                    }}>
+                        <View style={{ justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap' }}>
+                            <Text style={{
+                                color: Theme.textColor,
+                                fontWeight: '600',
+                                fontSize: 16
+                            }}>
+                                {t('products.staking.info.cooldownTitle')}
+                            </Text>
+                            <Text style={{
+                                color: Theme.success,
+                                fontSize: 16,
+                                fontVariant: ['tabular-nums']
+                            }}
+                                ellipsizeMode="tail"
+                                numberOfLines={1}
+                            >
+                                {t('products.staking.info.cooldownActive', { duration: cooldownDuration })}
+                            </Text>
+                        </View>
+                        <Text style={{
+                            color: Theme.textSecondary,
+                            fontWeight: '400',
+                            fontSize: 13,
+                        }}>
+                            {t('products.staking.info.cooldownDescription')}
+                        </Text>
+                    </View>
+                </>
+            )}
         </View>
     )
 })
--- app/components/Staking/StakingCycleProgress.tsx
+++ app/components/Staking/StakingCycleProgress.tsx
@@ -1,11 +1,27 @@
 import React, { useEffect, useRef } from "react"
 import { StyleProp, View, ViewStyle } from "react-native"
-import Animated, { call, Easing, measure, runOnUI, useAnimatedStyle, useCode, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import { AppConfig } from "../../AppConfig";
 import { Theme } from "../../Theme";
 
-export const StakingCycleProgress = React.memo(({ stakeUntil, style }: { stakeUntil: number, style?: StyleProp<ViewStyle> }) => {
-    const left = stakeUntil - (Date.now() / 1000);
-    const progress = 100 - Math.floor((left * 100) / (36 * 60 * 60));
+export const stakingCycle = AppConfig.isTestnet ? 7 * 60 * 60 + 30 * 60 : 36 * 60 * 60;
+
+export const StakingCycleProgress = React.memo((
+    {
+        left,
+        style,
+        color,
+        full,
+        reverse
+    }: {
+        left: number,
+        style?: StyleProp<ViewStyle>,
+        color?: string,
+        full?: number,
+        reverse?: boolean
+    }
+) => {
+    const progress = 100 - Math.floor((left * 100) / (full ? full : stakingCycle));
     const ref = useRef<Animated.View>(null);
     const scale = useSharedValue(0);
 
@@ -30,21 +46,28 @@ export const StakingCycleProgress = React.memo(({ stakeUntil, style }: { stakeUn
                 top: 0, left: 0, bottom: 0,
             }, style, { ...progressStyle }]}
         >
-            <View
+            {!reverse && (<View
                 style={[{
-                    backgroundColor: Theme.accent,
+                    backgroundColor: color ? color : Theme.accent,
                     height: 4,
                     width: '100%',
                 }]}
-            />
+            />)}
             <View
                 style={[{
-                    backgroundColor: Theme.accent,
+                    backgroundColor: color ? color : Theme.accent,
                     flexGrow: 1,
                     opacity: 0.1,
                     width: '100%'
                 }]}
             />
+            {reverse && (<View
+                style={[{
+                    backgroundColor: color ? color : Theme.accent,
+                    height: 4,
+                    width: '100%',
+                }]}
+            />)}
         </Animated.View>
     );
 })

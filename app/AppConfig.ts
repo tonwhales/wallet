4a84d6e2a79df777409e8b2cb5aafe35061f0d56
vzhovnitsky <vzhovnitsky@gmail.com>
2022-11-29 16:12:50 +0600

    wip: adding debug bundleId

--- app/AppConfig.ts
+++ app/AppConfig.ts
@@ -7,6 +7,7 @@ export const AppConfig = {
         Application.applicationId === 'com.tonhub.app.debug.testnet' ||
         Application.applicationId === 'com.tonhub.wallet.testnet' ||
         Application.applicationId === 'com.tonhub.wallet.testnet.debug' ||
-        Application.applicationId === 'com.sandbox.app.zenpay.demo'
+        Application.applicationId === 'com.sandbox.app.zenpay.demo' ||
+        Application.applicationId === 'com.sandbox.app.zenpay.demo.debug'
     )
 };

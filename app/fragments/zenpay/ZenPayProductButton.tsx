5f7545becf9f6959075e1d694a8ffc60c8e9d285
vzhovnitsky <vzhovnitsky@gmail.com>
2023-02-07 16:47:49 +0600

    add: updating zenpay product button

--- app/fragments/zenpay/ZenPayProductButton.tsx
+++ app/fragments/zenpay/ZenPayProductButton.tsx
@@ -3,7 +3,9 @@ import React, { useCallback } from "react";
 import { TouchableHighlight, View, Text, useWindowDimensions, Image } from "react-native";
 import { PriceComponent } from "../../components/PriceComponent";
 import { ValueComponent } from "../../components/ValueComponent";
-import { ZenPayCard } from "../../engine/corp/ZenPayProduct";
+import { ZenPayCard, zenPayUrl } from "../../engine/corp/ZenPayProduct";
+import { Engine } from "../../engine/Engine";
+import { extractDomain } from "../../engine/utils/extractDomain";
 import { t } from "../../i18n/t";
 import { Theme } from "../../Theme";
 import { avatarHash } from "../../utils/avatarHash";
@@ -16,16 +18,44 @@ const colorsMap = [
     ['#792AF6', "#954CF9"],
 ];
 
-export const ZenPayProductButton = React.memo(({ card }: { card?: ZenPayCard }) => {
+export const ZenPayProductButton = React.memo(({ card, engine }: { card?: ZenPayCard, engine: Engine }) => {
     const dimentions = useWindowDimensions();
     const navigation = useTypedNavigation();
     const fontScaleNormal = dimentions.fontScale <= 1;
+    const status = engine.products.zenPay.useStatus();
+
+    const needsEnrolment = React.useMemo(() => {
+        try {
+            let domain = extractDomain(zenPayUrl);
+            if (!domain) {
+                return; // Shouldn't happen
+            }
+            let domainKey = engine.persistence.domainKeys.getValue(domain);
+            if (!domainKey) {
+                return true;
+            }
+            if (status.state === 'need-enrolment') {
+                return true;
+            }
+        } catch (error) {
+            return true;
+        }
+        return false;
+    }, [status]);
 
     const onPress = useCallback(
         () => {
+            if (needsEnrolment) {
+                navigation.navigateZenPayEnrollment({
+                    endpoint: zenPayUrl, callback: () => {
+                        navigation.navigateZenPay({ type: 'about' });
+                    }
+                });
+                return;
+            }
             navigation.navigateZenPay(card ? { type: 'card', id: card.id } : { type: 'account' });
         },
-        [card],
+        [card, needsEnrolment],
     );
 
     const colors = card ? colorsMap[avatarHash(card.id, colorsMap.length)] : ['#333A5A', "#A7AFD3"]
@@ -75,7 +105,7 @@ export const ZenPayProductButton = React.memo(({ card }: { card?: ZenPayCard }) 
                     <View style={{ flexDirection: 'column', flexGrow: 1, flexBasis: 0 }}>
                         <View style={{ flexDirection: 'row', alignItems: 'baseline', justifyContent: 'space-between', marginTop: 10, marginRight: 10 }}>
                             <Text style={{ color: Theme.textColor, fontSize: 16, marginRight: 16, fontWeight: '600', flexShrink: 1 }} ellipsizeMode="tail" numberOfLines={fontScaleNormal ? 1 : 2}>
-                                {card.card.lastFourDigits ? t('products.zenPay.card.title', {cardNumber: card.card.lastFourDigits}) : t('products.zenPay.card.defaultTitle')}
+                                {card.card.lastFourDigits ? t('products.zenPay.card.title', { cardNumber: card.card.lastFourDigits }) : t('products.zenPay.card.defaultTitle')}
                             </Text>
                             {!!card && card.balance && (
                                 <Text style={{ color: Theme.textColor, fontWeight: '400', fontSize: 16, marginRight: 2, alignSelf: 'flex-start' }}>

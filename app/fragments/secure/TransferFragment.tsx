32749dd9e0387dfb5b9dd0b92b04f8de263c794b
vzhovnitsky <vzhovnitsky@gmail.com>
2023-03-21 14:10:09 +0200

    fix: fixing transfer jetton check

--- app/fragments/secure/TransferFragment.tsx
+++ app/fragments/secure/TransferFragment.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { Platform, View, Alert } from "react-native";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Address, Cell, CellMessage, CommentMessage, CommonMessageInfo, ExternalMessage, fromNano, InternalMessage, SendMode, StateInit } from 'ton';
+import { Address, Cell, CellMessage, CommentMessage, CommonMessageInfo, ExternalMessage, fromNano, InternalMessage, parseSupportedMessage, resolveKnownInterface, SendMode, StateInit } from 'ton';
 import { AndroidToolbar } from '../../components/AndroidToolbar';
 import { contractFromPublicKey } from '../../engine/contractFromPublicKey';
 import { backoff } from '../../utils/time';
@@ -27,6 +27,8 @@ import { TransferSingle } from './components/TransferSingle';
 import { TransferBatch } from './components/TransferBatch';
 import { createWalletTransferV4, internalFromSignRawMessage } from '../../engine/utils/createWalletTransferV4';
 import { AppConfig } from '../../AppConfig';
+import { parseBody } from '../../engine/transactions/parseWalletTransaction';
+import { parseMessageBody } from '../../engine/transactions/parseMessageBody';
 
 export type ATextInputRef = {
     focus: () => void;
@@ -222,9 +224,22 @@ export const TransferFragment = fragment(() => {
                 // Read jetton master
                 let jettonMaster: JettonMasterState | null = null;
                 if (metadata.jettonWallet) {
-                    const stored = engine.persistence.jettonMasters.item(metadata.jettonWallet!.master).value;
-                    if (stored && !!stored.name && !!stored.symbol) {
-                        jettonMaster = stored;
+                    let body = order.messages[0].payload ? parseBody(order.messages[0].payload) : null;
+                    if (body && body.type === 'payload') {
+                        const temp = order.messages[0].payload;
+                        let sc = temp?.beginParse();
+                        if (sc) {
+                            if (sc.remaining > 32) {
+                                let op = sc.readUintNumber(32);
+                                // Jetton transfer op
+                                if (op === 0xf8a7ea5) {
+                                    jettonMaster = engine
+                                        .persistence
+                                        .jettonMasters
+                                        .item(metadata.jettonWallet!.master).value;
+                                }
+                            }
+                        }
                     }
                 }
 

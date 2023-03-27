2f9d284780c33f9d70b6d5fdf79a8eda81e548df
vzhovnitsky <vzhovnitsky@gmail.com>
2023-03-27 09:50:51 +0300

    Merge branch 'develop' into hub-120-feat-lock-ups
    # Conflicts:
    #	app/engine/utils/codecs.ts

--- app/engine/utils/codecs.ts
+++ app/engine/utils/codecs.ts
@@ -132,25 +132,25 @@ export class EnumType<A> extends t.Type<A> {
     public readonly _tag: 'EnumType' = 'EnumType'
     public enumObject!: object
     public constructor(e: object, name?: string) {
-      super(
-        name || 'enum',
-        (u): u is A => {
-          if (!Object.values(this.enumObject).find((v) => v === u)) {
-            return false
-          }
-          // enum reverse mapping check
-          if (typeof (this.enumObject as any)[u as string] === 'number') {
-            return false
-          }
-  
-          return true
-        },
-        (u, c) => (this.is(u) ? t.success(u) : t.failure(u, c)),
-        t.identity,
-      )
-      this.enumObject = e
+        super(
+            name || 'enum',
+            (u): u is A => {
+                if (!Object.values(this.enumObject).find((v) => v === u)) {
+                    return false
+                }
+                // enum reverse mapping check
+                if (typeof (this.enumObject as any)[u as string] === 'number') {
+                    return false
+                }
+
+                return true
+            },
+            (u, c) => (this.is(u) ? t.success(u) : t.failure(u, c)),
+            t.identity,
+        )
+        this.enumObject = e
     }
-  }
-  
-  // simple helper function
-  export const createEnumType = <T>(e: object, name?: string) => new EnumType<T>(e, name)
+}
+
+// simple helper function
+export const createEnumType = <T>(e: object, name?: string) => new EnumType<T>(e, name)

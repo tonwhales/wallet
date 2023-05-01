ca65fabcd1a78d462c1ff703ac1341d1b5e8223b
vzhovnitsky <vzhovnitsky@gmail.com>
2023-05-01 12:06:33 +0300

    cleanup: removed android NDK downgrade and useless commented code

--- android/app/src/main/java/com/tonhub/wallet/MainActivity.java
+++ android/app/src/main/java/com/tonhub/wallet/MainActivity.java
@@ -64,18 +64,4 @@ public class MainActivity extends ReactActivity {
             DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
     ));
   }
-
-//  public static class MainActivityDelegate extends ReactActivityDelegate {
-//    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-//      super(activity, mainComponentName);
-//    }
-//
-//    @Override
-//    protected ReactRootView createRootView() {
-//      ReactRootView reactRootView = new ReactRootView(getContext());
-//      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-//      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-//      return reactRootView;
-//    }
-//  }
 }

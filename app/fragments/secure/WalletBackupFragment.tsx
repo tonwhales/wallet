ab532a5656ec384862a17a4ae003d23200f573d7
vzhovnitsky <vzhovnitsky@gmail.com>
2023-06-26 14:28:02 +0300

    cleanup: rm useless passcode reset fragment

--- app/fragments/secure/WalletBackupFragment.tsx
+++ app/fragments/secure/WalletBackupFragment.tsx
@@ -15,7 +15,6 @@ import { useAppConfig } from '../../utils/AppConfigContext';
 import { useKeysAuth } from '../../components/secure/AuthWalletKeys';
 import { useReboot } from '../../utils/RebootContext';
 import { warn } from '../../utils/log';
-import { BiometricsState, getBiometricsState } from '../../storage/secureStorage';
 
 export const WalletBackupFragment = systemFragment(() => {
     const safeArea = useSafeAreaInsets();

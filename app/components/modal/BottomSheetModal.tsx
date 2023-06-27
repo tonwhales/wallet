073a90fabeed24fb766d4925dcfa974776e0785a
vzhovnitsky <vzhovnitsky@gmail.com>
2023-06-27 18:21:57 +0300

    wip: adding BottomSheetModal on backdrop press hide

--- app/components/modal/BottomSheetModal.tsx
+++ app/components/modal/BottomSheetModal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { useAppConfig } from "../../utils/AppConfigContext";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 
 // BottomSheet provider with hooks to show/hide the bottom sheet with passed component
 type BottomSheetProviderProps = {
@@ -22,12 +22,13 @@ export const BottomSheetProvider = React.memo((props: BottomSheetProviderProps) 
     const { Theme } = useAppConfig();
     const safeArea = useSafeAreaInsets();
     const [state, setState] = React.useState<BottomSheetProviderState>();
+    const sheetRef = React.useRef<BottomSheet>(null);
 
     const show = (component: React.ReactNode, snapPoints: (string | number)[]) => {
         setState({ visible: true, component, snapPoints });
     };
     const hide = () => {
-        setState({ visible: false, component: null, snapPoints: null });
+        setState({ visible: false, component: null, snapPoints: ['100%'] });
     };
 
     return (
@@ -39,24 +40,35 @@ export const BottomSheetProvider = React.memo((props: BottomSheetProviderProps) 
         >
             {props.children}
             {state && (
-                <BottomSheet
-                    index={state.visible ? 0 : -1}
-                    snapPoints={state.snapPoints ?? [-1]}
-                    enablePanDownToClose={true}
-                    keyboardBehavior="interactive"
-                    keyboardBlurBehavior="restore"
-                    style={{ backgroundColor: 'white', borderTopRightRadius: 20, borderTopLeftRadius: 20 }}
-                    containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', paddingTop: safeArea.top }}
-                    onClose={() => {
-                        setState(undefined);
-                    }}
-                >
-                    <BottomSheetView
-                        pointerEvents="box-none"
+                <>
+                    <Pressable
+                        onPress={() => {
+                            sheetRef.current?.close();
+                        }}
+                        style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
+                    />
+                    <BottomSheet
+                        ref={sheetRef}
+                        index={state.visible ? 0 : -1}
+                        snapPoints={state.snapPoints ?? ['95%']}
+                        enablePanDownToClose={true}
+                        keyboardBehavior="interactive"
+                        keyboardBlurBehavior="restore"
+                        style={{ backgroundColor: 'white', borderTopRightRadius: 20, borderTopLeftRadius: 20 }}
+                        containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', paddingTop: safeArea.top }}
+                        onChange={(index) => {
+                            if (index === -1) {
+                                setState(undefined);
+                            }
+                        }}
                     >
-                        {state?.component}
-                    </BottomSheetView>
-                </BottomSheet>
+                        <BottomSheetView
+                            pointerEvents="box-none"
+                        >
+                            {state.component}
+                        </BottomSheetView>
+                    </BottomSheet>
+                </>
             )}
         </BottomSheetContext.Provider>
     );

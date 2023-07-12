4940d67bf7398c9edf82aa2cd77516dc64837892
vzhovnitsky <vzhovnitsky@gmail.com>
2023-07-12 17:41:56 +0300

    fix: modal getting stuck

--- app/components/modal/BottomSheetModal.tsx
+++ app/components/modal/BottomSheetModal.tsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { createContext, memo, useContext, useEffect, useRef, useState } from "react";
 import BottomSheet, { BottomSheetView, BottomSheetFooter } from "@gorhom/bottom-sheet";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Pressable } from "react-native";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
 
-// BottomSheet provider with hooks to show/hide the bottom sheet with passed component
 type BottomSheetProviderProps = {
     children: React.ReactNode;
 };
@@ -13,15 +12,14 @@ type BottomSheetProviderState = {
     snapPoints: (string | number)[] | null,
     footer?: any
 };
-const BottomSheetContext = React.createContext<{
+const BottomSheetContext = createContext<{
     show: (component: React.ReactNode, snapPoints: (string | number)[], footer?: any) => void;
     hide: () => void;
 } | null>(null);
 
-export const BottomSheetProvider = React.memo((props: BottomSheetProviderProps) => {
-    const safeArea = useSafeAreaInsets();
-    const [state, setState] = React.useState<BottomSheetProviderState>();
-    const sheetRef = React.useRef<BottomSheet>(null);
+export const BottomSheetProvider = memo((props: BottomSheetProviderProps) => {
+    const [state, setState] = useState<BottomSheetProviderState>();
+    const sheetRef = useRef<BottomSheet>(null);
 
     const show = (component: React.ReactNode, snapPoints: (string | number)[], footer?: any) => {
         setState({ visible: true, component, snapPoints, footer });
@@ -40,37 +38,52 @@ export const BottomSheetProvider = React.memo((props: BottomSheetProviderProps) 
             {props.children}
             {state && (
                 <>
-                    <Pressable
-                        onPress={() => {
-                            sheetRef.current?.close();
-                        }}
-                        style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
-                    />
+                    {state.visible && (
+                        <Animated.View
+                            entering={FadeIn}
+                            exiting={FadeOut}
+                            style={{
+                                position: 'absolute',
+                                top: 0, left: 0, right: 0, bottom: 0,
+                                backgroundColor: 'rgba(0, 0, 0, 0.6)'
+                            }}
+                        >
+                            <Pressable
+                                onPress={() => {
+                                    sheetRef.current?.close();
+                                    hide();
+                                }}
+                                style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}
+                            />
+                        </Animated.View>
+                    )}
                     <BottomSheet
                         ref={sheetRef}
-                        index={state.visible ? 0 : -1}
-                        snapPoints={state.snapPoints ?? ['95%']}
+                        index={state?.visible ? 0 : -1}
+                        snapPoints={state?.snapPoints ?? ['95%']}
                         enablePanDownToClose={true}
-                        keyboardBehavior="interactive"
-                        keyboardBlurBehavior="restore"
+                        keyboardBehavior={'interactive'}
+                        keyboardBlurBehavior={'restore'}
                         handleComponent={null}
                         style={{ backgroundColor: 'white', borderTopRightRadius: 20, borderTopLeftRadius: 20 }}
-                        containerStyle={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', paddingTop: safeArea.top }}
+                        onClose={() => {
+                            hide();
+                        }}
                         onChange={(index) => {
                             if (index === -1) {
                                 setState(undefined);
                             }
                         }}
-                        footerComponent={state.footer ? (props) => {
+                        footerComponent={state?.footer ? (props) => {
                             return (
                                 <BottomSheetFooter {...props}>
-                                    {state.footer}
+                                    {state?.footer}
                                 </BottomSheetFooter>
                             );
                         } : undefined}
                     >
                         <BottomSheetView pointerEvents="box-none">
-                            {state.component}
+                            {state?.component}
                         </BottomSheetView>
                     </BottomSheet>
                 </>
@@ -80,5 +93,5 @@ export const BottomSheetProvider = React.memo((props: BottomSheetProviderProps) 
 });
 
 export function useBottomSheet() {
-    return React.useContext(BottomSheetContext);
+    return useContext(BottomSheetContext);
 }

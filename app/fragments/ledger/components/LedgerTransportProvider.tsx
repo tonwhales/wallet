60e25f060d6fb24e55b6a836eb6ad50497085d23
vzhovnitsky <vzhovnitsky@gmail.com>
2023-07-25 14:30:09 +0300

    wip: new ble search flow

--- app/fragments/ledger/components/LedgerTransportProvider.tsx
+++ app/fragments/ledger/components/LedgerTransportProvider.tsx
@@ -1,7 +1,7 @@
 import Transport from "@ledgerhq/hw-transport";
 import TransportHID from "@ledgerhq/react-native-hid";
 import TransportBLE from "@ledgerhq/react-native-hw-transport-ble";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 import { TonTransport } from "ton-ledger";
 import { useTypedNavigation } from "../../../utils/useTypedNavigation";
 import { Alert, Platform } from "react-native";
@@ -12,12 +12,56 @@ import { Address } from "ton";
 import { warn } from "../../../utils/log";
 import { useEngine } from "../../../engine/Engine";
 import { checkMultiple, PERMISSIONS, requestMultiple } from 'react-native-permissions';
-import * as Application from 'expo-application';
-import * as IntentLauncher from 'expo-intent-launcher';
 
 export type TypedTransport = { type: 'hid' | 'ble', transport: Transport }
 export type LedgerAddress = { acc: number, address: string, publicKey: Buffer };
-export type BLESearchState = { type: 'ongoing' } | { type: 'completed', success: boolean } | { type: 'permissions-failed' }
+
+export type BLESearchState =
+    | { type: 'ongoing', devices: any[] }
+    | { type: 'completed', success: true, devices: any[] }
+    | { type: 'completed', success: false, }
+    | { type: 'permissions-failed' }
+    | null;
+
+type BleSearchAction =
+    | { type: 'ongoing' }
+    | { type: 'add', device: any }
+    | { type: 'error' }
+    | { type: 'complete' }
+    | { type: 'start' }
+    | { type: 'permissions-failed' }
+
+const bleSearchStateReducer = (state: BLESearchState, action: BleSearchAction): BLESearchState => {
+    switch (action.type) {
+        case 'start':
+            return {
+                type: 'ongoing',
+                devices: [],
+            };
+        case 'add':
+            return {
+                type: 'ongoing',
+                devices: state?.type === 'ongoing' ? state.devices.concat(action.device) : [action.device],
+            };
+        case 'complete':
+            return {
+                type: 'completed',
+                success: true,
+                devices: state?.type === 'ongoing' ? state.devices : [],
+            };
+        case 'error':
+            return {
+                type: 'completed',
+                success: false,
+            }
+        case 'permissions-failed':
+            return {
+                type: 'permissions-failed',
+            }
+        default:
+            return state;
+    }
+}
 
 export const TransportContext = React.createContext<
     {
@@ -25,7 +69,10 @@ export const TransportContext = React.createContext<
         setLedgerConnection: (transport: TypedTransport | null) => void,
         tonTransport: TonTransport | null,
         addr: LedgerAddress | null,
-        setAddr: (addr: LedgerAddress | null) => void
+        setAddr: (addr: LedgerAddress | null) => void,
+        bleSearchState: BLESearchState,
+        startHIDSearch: () => Promise<void>,
+        startBleSearch: () => void,
     }
     | null
 >(null);
@@ -36,9 +83,8 @@ export const LedgerTransportProvider = ({ children }: { children: React.ReactNod
     const [ledgerConnection, setLedgerConnection] = useState<TypedTransport | null>(null);
     const [tonTransport, setTonTransport] = useState<TonTransport | null>(null);
     const [addr, setAddr] = useState<LedgerAddress | null>(null);
-    const [scan, setScan] = useState<BLESearchState | null>(null);
-    const [devices, setDevices] = useState([]);
-    const [search, setSearch] = useState(0);
+    const [bleState, dispatchBleState] = useReducer(bleSearchStateReducer, null);
+    const [bleSearch, setSearch] = useState(0);
 
     const reset = useCallback(() => {
         setLedgerConnection(null);
@@ -70,95 +116,99 @@ export const LedgerTransportProvider = ({ children }: { children: React.ReactNod
         }
     }, []);
 
-    const startBLESearch = useCallback(async () => {
+    const startHIDSearch = useCallback(async () => {
+        let hid = await TransportHID.create();
+        setLedgerConnection({ type: 'hid', transport: hid });
+    }, []);
+
+    const startBleSearch = useCallback(async () => {
+        setSearch(bleSearch + 1);
+    }, [bleSearch]);
+
+    useEffect(() => {
         let powerSub: Subscription;
         let sub: Subscription;
         const scan = async () => {
-            setScan({ type: 'ongoing' });
+            dispatchBleState({ type: 'start' });
             sub = new Observable(TransportBLE.listen).subscribe({
                 complete: () => {
-                    setScan({ type: 'completed', success: true });
+                    dispatchBleState({ type: 'complete' });
                 },
                 next: e => {
                     if (e.type === "add") {
                         const device = e.descriptor;
-                        setDevices((prev) => {
-                            if (prev.some((i: any) => i.id === device.id)) {
-                                return prev;
-                            }
-                            return devices.concat(device);
-                        });
+                        dispatchBleState({ type: 'add', device });
                     }
                     // NB there is no "remove" case in BLE.
                 },
                 error: error => {
-                    setScan({ type: 'completed', success: false });
+                    dispatchBleState({ type: 'error' });
                 }
             });
         }
-        if (Platform.OS === "android" && Platform.Version >= 23) {
-            const checkCoarse = await checkMultiple([
-                PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
-                PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
-            ]);
-
-            if (checkCoarse[PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION] !== 'granted'
-                || checkCoarse[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION] !== 'granted') {
-                const requestLocation = await requestMultiple([
+        (async () => {
+            if (Platform.OS === "android" && Platform.Version >= 23) {
+                const checkCoarse = await checkMultiple([
                     PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
                     PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
                 ]);
-                if (requestLocation[PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION] !== 'granted'
-                    || requestLocation[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION] !== 'granted') {
-                    setScan({ type: 'permissions-failed' });
-                    return;
+
+                if (checkCoarse[PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION] !== 'granted'
+                    || checkCoarse[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION] !== 'granted') {
+                    const requestLocation = await requestMultiple([
+                        PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+                        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
+                    ]);
+                    if (requestLocation[PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION] !== 'granted'
+                        || requestLocation[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION] !== 'granted') {
+                        dispatchBleState({ type: 'permissions-failed' });
+                        return;
+                    }
                 }
-            }
 
-            const scanConnect = await checkMultiple([
-                PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
-                PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
-            ]);
-
-            if (scanConnect[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] !== 'granted'
-                || scanConnect[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] !== 'granted') {
-                let resScanConnect = await requestMultiple([
+                const scanConnect = await checkMultiple([
                     PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
                     PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
                 ]);
-                if (resScanConnect[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] !== 'granted'
-                    || resScanConnect[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] !== 'granted') {
-                    setScan({ type: 'permissions-failed' });
-                    return;
-                }
-            }
-        }
 
-        let previousAvailable = false;
-        powerSub = new Observable(TransportBLE.observeState).subscribe((e: any) => {
-            if (e.type === 'PoweredOff') {
-                Alert.alert(t('hardwareWallet.errors.turnOnBluetooth'));
-                if (sub) sub.unsubscribe();
-                setDevices([]);
-                setScan(null);
-                reset();
-            }
-            if (e.available !== previousAvailable) {
-                previousAvailable = e.available;
-                if (e.available) {
-                    if (sub) sub.unsubscribe();
-                    setDevices([]);
-                    setScan(null);
-                    scan();
+                if (scanConnect[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] !== 'granted'
+                    || scanConnect[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] !== 'granted') {
+                    let resScanConnect = await requestMultiple([
+                        PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+                        PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+                    ]);
+                    if (resScanConnect[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] !== 'granted'
+                        || resScanConnect[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] !== 'granted') {
+                        dispatchBleState({ type: 'permissions-failed' });
+                        return;
+                    }
                 }
             }
-        });
+
+            let previousAvailable = false;
+            powerSub = new Observable(TransportBLE.observeState).subscribe((e: any) => {
+                if (e.type === 'PoweredOff') {
+                    Alert.alert(t('hardwareWallet.errors.turnOnBluetooth'));
+                    if (sub) sub.unsubscribe();
+                    dispatchBleState({ type: 'error' });
+                    reset();
+                }
+                if (e.available !== previousAvailable) {
+                    previousAvailable = e.available;
+                    if (e.available) {
+                        if (sub) sub.unsubscribe();
+                        dispatchBleState({ type: 'error' });
+                        scan();
+                    }
+                }
+            });
+        })();
 
         return () => {
             if (sub) sub.unsubscribe();
             if (powerSub) powerSub.unsubscribe();
         }
-    }, []);
+    }, [bleSearch]);
 
     useEffect(() => {
         let sub: Subscription | null = null;
@@ -189,7 +239,17 @@ export const LedgerTransportProvider = ({ children }: { children: React.ReactNod
     }, [ledgerConnection]);
 
     return (
-        <TransportContext.Provider value={{ ledgerConnection, setLedgerConnection: onSetLedgerConnecton, tonTransport, addr, setAddr: onSetAddress }}>
+        <TransportContext.Provider
+            value={{
+                ledgerConnection,
+                setLedgerConnection: onSetLedgerConnecton,
+                tonTransport,
+                addr,
+                setAddr: onSetAddress,
+                startHIDSearch,
+                startBleSearch,
+                bleSearchState: bleState,
+            }}>
             {children}
         </TransportContext.Provider>
     );

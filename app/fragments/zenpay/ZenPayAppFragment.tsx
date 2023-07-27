1a772de778b3f96b2da6af0ffd2b6978499aad3c
vzhovnitsky <vzhovnitsky@gmail.com>
2023-07-27 12:57:55 +0300

    fix: fixing account status fetching & route navigation

--- app/fragments/zenpay/ZenPayAppFragment.tsx
+++ app/fragments/zenpay/ZenPayAppFragment.tsx
@@ -25,7 +25,7 @@ export const ZenPayAppFragment = fragment(() => {
     const endpoint = useMemo(() => {
         let url = zenPayUrl;
         if (params.type === 'account') {
-            url += status.state === 'ok' ? '/create' : '/about';
+            url += status.state === 'ok' ? '/create' : '/';
         } else if (params.type === 'card') {
             url += `/card/${params.id}`;
         }

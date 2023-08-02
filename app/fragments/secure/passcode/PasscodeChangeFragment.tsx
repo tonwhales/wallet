5455fab7ce557625e59127c3952d01317e76d87f
vzhovnitsky <vzhovnitsky@gmail.com>
2023-08-02 17:04:15 +0300

    wip: fixing PasscodeChangeFragment header

--- app/fragments/secure/passcode/PasscodeChangeFragment.tsx
+++ app/fragments/secure/passcode/PasscodeChangeFragment.tsx
@@ -1,14 +1,15 @@
-import { Platform, View, Text } from "react-native"
+import { Platform, View } from "react-native"
 import { PasscodeChange } from "../../../components/secure/PasscodeChange";
 import { fragment } from "../../../fragment"
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
-import { CloseButton } from "../../../components/CloseButton";
 import { t } from "../../../i18n/t";
-import { AndroidToolbar } from "../../../components/topbar/AndroidToolbar";
+import { ScreenHeader } from "../../../components/ScreenHeader";
+import { useNavigation } from "@react-navigation/native";
 
 export const PasscodeChangeFragment = fragment(() => {
     const safeArea = useSafeAreaInsets();
+    const navigation = useNavigation();
 
     return (
         <View style={{
@@ -16,22 +17,8 @@ export const PasscodeChangeFragment = fragment(() => {
             paddingTop: Platform.OS === 'android' ? safeArea.top : undefined,
         }}>
             <StatusBar style={Platform.OS === 'ios' ? 'light' : 'dark'} />
-            <AndroidToolbar pageTitle={t('security.passcodeSettings.changeTitle')} />
-            {Platform.OS === 'ios' && (
-                <View style={{
-                    marginTop: 17,
-                    height: 32
-                }}>
-                    <Text style={[{
-                        fontWeight: '600',
-                        fontSize: 17
-                    }, { textAlign: 'center' }]}>
-                        {t('security.passcodeSettings.changeTitle')}
-                    </Text>
-                </View>
-            )}
+            <ScreenHeader title={t('security.passcodeSettings.changeTitle')} onClosePressed={navigation.goBack} />
             <PasscodeChange />
-            <CloseButton style={{ position: 'absolute', top: 22, right: 16 }} />
         </View>
     );
 });

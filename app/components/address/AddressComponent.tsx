296154ade9b4f5f68b86e2ace8400961ecdb9ccf
vzhovnitsky <vzhovnitsky@gmail.com>
2023-09-04 18:04:35 +0300

    fix: fixing short address length

--- app/components/address/AddressComponent.tsx
+++ app/components/address/AddressComponent.tsx
@@ -5,5 +5,5 @@ import { useAppConfig } from "../../utils/AppConfigContext";
 export function AddressComponent(props: { address: Address, start?: number, end?: number }) {
     const { AppConfig } = useAppConfig();
     let t = props.address.toFriendly({ testOnly: AppConfig.isTestnet });
-    return <Text>{t.slice(0, props.start ?? 10) + '...' + t.slice(t.length - (props.end ?? 6))}</Text>;
+    return <Text>{t.slice(0, props.start ?? 4) + '...' + t.slice(t.length - (props.end ?? 4))}</Text>;
 }
--- app/utils/shortAddress.ts
+++ app/utils/shortAddress.ts
@@ -3,10 +3,10 @@ import { Address } from "ton";
 export function shortAddress({ isTestnet, address, friendly }: { isTestnet: boolean, address?: Address, friendly?: string }) {
     if (address) {
         let t = address.toFriendly({ testOnly: isTestnet });
-        return t.slice(0, 2) + '...' + t.slice(t.length - 6);
+        return t.slice(0, 4) + '...' + t.slice(t.length - 4);
     }
     if (friendly) {
-        return friendly.slice(0, 2) + '...' + friendly.slice(friendly.length - 6);
+        return friendly.slice(0, 4) + '...' + friendly.slice(friendly.length - 6);
     }
     return '';
 }

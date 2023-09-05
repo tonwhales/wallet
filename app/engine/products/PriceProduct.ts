87f574afa57ceb4a9be99fb3bef80b299d0ce25b
vzhovnitsky <vzhovnitsky@gmail.com>
2023-09-05 13:17:21 +0300

    wip: adding currency selector labels

--- app/engine/products/PriceProduct.ts
+++ app/engine/products/PriceProduct.ts
@@ -20,17 +20,17 @@ export const PrimaryCurrency: { [key: string]: string } = {
     Jpy: 'JPY',
 }
 
-export const CurrencySymbols: { [key: string]: { symbol: string, end?: boolean } } = {
-    [PrimaryCurrency.Usd]: { symbol: '$' },
-    [PrimaryCurrency.Eur]: { symbol: '€', end: true },
-    [PrimaryCurrency.Rub]: { symbol: '₽', end: true },
-    [PrimaryCurrency.Gbp]: { symbol: '£' },
-    [PrimaryCurrency.Chf]: { symbol: '₣' },
-    [PrimaryCurrency.Cny]: { symbol: '¥' },
-    [PrimaryCurrency.Krw]: { symbol: '₩' },
-    [PrimaryCurrency.Idr]: { symbol: 'Rp', end: true },
-    [PrimaryCurrency.Inr]: { symbol: '₹' },
-    [PrimaryCurrency.Jpy]: { symbol: '¥' },
+export const CurrencySymbols: { [key: string]: { symbol: string, end?: boolean, label: string } } = {
+    [PrimaryCurrency.Usd]: { symbol: '$', label: 'U.S. Dollar' },
+    [PrimaryCurrency.Eur]: { symbol: '€', end: true, label: 'Euro' },
+    [PrimaryCurrency.Rub]: { symbol: '₽', end: true, label: 'Russian Ruble' },
+    [PrimaryCurrency.Gbp]: { symbol: '£', label: 'British Pound' },
+    [PrimaryCurrency.Chf]: { symbol: '₣', label: 'Swiss Franc' },
+    [PrimaryCurrency.Cny]: { symbol: '¥', label: 'Chinese Yuan' },
+    [PrimaryCurrency.Krw]: { symbol: '₩', label: 'South Korean Won' },
+    [PrimaryCurrency.Idr]: { symbol: 'Rp', end: true, label: 'Indonesian Rupiah' },
+    [PrimaryCurrency.Inr]: { symbol: '₹', label: 'Indian Rupee' },
+    [PrimaryCurrency.Jpy]: { symbol: '¥', label: 'Japanese Yen' },
 };
 
 export type PriceState = {

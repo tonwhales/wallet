a3a24f91d0cc1c60654fb32a2b8bfba41d1353fd
Dan Volkov <hi@dvlkv.com>
2023-11-01 13:44:42 +0400

    wip: patches

--- app/engine/hooks/useJettonMaster.ts
+++ app/engine/hooks/useJettonMaster.ts
@@ -1,6 +1,4 @@
-import { Address } from '@ton/core';
 import { useJettonContent } from './basic/useJettonContent';
-import { useNetwork } from './useNetwork';
 
 export function useJettonMaster(address: string | null) {
     return useJettonContent(address);

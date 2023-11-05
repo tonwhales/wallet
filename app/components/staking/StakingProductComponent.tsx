cdf57ab6709777e9369f97b096bc8ac62e38d4d1
Dan Volkov <hi@dvlkv.com>
2023-11-06 02:26:10 +0400

    fix: components case

---
+++ app/components/staking/StakingProductComponent.tsx
@@ -0,0 +1,150 @@
+import React, { memo, useMemo } from "react";
+import { useTypedNavigation } from "../../utils/useTypedNavigation";
+import { TouchableHighlight, View, Text, useWindowDimensions } from "react-native";
+import StakingIcon from '../../../assets/ic_staking.svg';
+import { PriceComponent } from "../PriceComponent";
+import { t } from "../../i18n/t";
+import { ValueComponent } from "../ValueComponent";
+import { useStaking } from '../../engine/hooks';
+import { useStakingApy } from '../../engine/hooks';
+import { useNetwork } from '../../engine/hooks';
+import { useTheme } from '../../engine/hooks';
+
+export const StakingProductComponent = memo(() => {
+    const theme = useTheme();
+    const { isTestnet } = useNetwork();
+    const navigation = useTypedNavigation();
+    const staking = useStaking();
+    const showJoin = staking.total === 0n;
+
+    const apy = useStakingApy()?.apy;
+    const apyWithFee = useMemo(() => {
+        if (!!apy) {
+            return (apy - apy * (5 / 100)).toFixed(2)
+        }
+    }, [apy]);
+
+    const dimentions = useWindowDimensions();
+    const fontScaleNormal = dimentions.fontScale <= 1;
+
+    if (!showJoin) return (
+        <TouchableHighlight
+            onPress={() => navigation.navigate('StakingPools')}
+            underlayColor={theme.selector}
+            style={{
+                alignSelf: 'stretch', borderRadius: 14,
+                backgroundColor: theme.item,
+                marginHorizontal: 16, marginVertical: 4
+            }}
+        >
+            <View style={{
+                padding: 10
+            }}>
+                <View style={{ alignSelf: 'stretch', flexDirection: 'row' }}>
+                    <View style={{ width: 42, height: 42, borderRadius: 21, borderWidth: 0, marginRight: 10 }}>
+                        <View style={{ backgroundColor: theme.success, borderRadius: 21, width: 42, height: 42, alignItems: 'center', justifyContent: 'center' }}>
+                            <StakingIcon width={42} height={42} color={'white'} />
+                        </View>
+                    </View>
+                    <View style={{
+                        flexDirection: 'column',
+                        flexGrow: 1,
+                        paddingVertical: 2,
+                    }}>
+                        <View style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            marginBottom: 3
+                        }}>
+                            <Text style={{ color: theme.textColor, fontSize: 16, marginRight: 16, fontWeight: '600' }} ellipsizeMode="tail" numberOfLines={1}>
+                                {t('products.staking.title')}
+                            </Text>
+                            <Text style={{
+                                fontWeight: '400',
+                                fontSize: 16,
+                                color: staking.total && staking.total > BigInt(0)
+                                    ? theme.pricePositive
+                                    : theme.textColor
+                            }}>
+                                <ValueComponent
+                                    value={staking.total}
+                                    precision={3}
+                                />
+                            </Text>
+                        </View>
+                        <View style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                        }}>
+                            <Text style={{ color: theme.price, fontSize: 13, fontWeight: '400' }} ellipsizeMode="tail">
+                                {t("products.staking.subtitle.joined", { apy: apyWithFee ?? '8' })}
+                            </Text>
+                            <PriceComponent
+                                amount={staking.total}
+                                style={{
+                                    backgroundColor: theme.transparent,
+                                    paddingHorizontal: 0, paddingVertical: 0,
+                                    alignSelf: 'flex-end',
+                                    marginTop: 2, height: undefined,
+                                    minHeight: fontScaleNormal ? 14 : undefined
+                                }}
+                                textStyle={{ color: theme.textSubtitle, fontWeight: '400', fontSize: 12 }}
+                            />
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </TouchableHighlight>
+    );
+
+    return (
+        <TouchableHighlight
+            onPress={() => navigation.navigate('StakingPools')}
+            underlayColor={theme.selector}
+            style={{
+                alignSelf: 'stretch', borderRadius: 14,
+                backgroundColor: theme.item,
+                marginHorizontal: 16, marginVertical: 4,
+            }}
+        >
+            <View style={{
+                padding: 10
+            }}>
+                <View style={{ alignSelf: 'stretch', flexDirection: 'row' }}>
+                    <View style={{ width: 42, height: 42, borderRadius: 21, borderWidth: 0, marginRight: 10, alignSelf: 'center' }}>
+                        <View style={{ backgroundColor: theme.success, borderRadius: 21, width: 42, height: 42, alignItems: 'center', justifyContent: 'center' }}>
+                            <StakingIcon width={42} height={42} color={'white'} />
+                        </View>
+                    </View>
+                    <View style={{
+                        flexDirection: 'row',
+                        flexGrow: 1,
+                        paddingVertical: 2,
+                        justifyContent: 'space-between',
+                    }}>
+                        <View>
+                            <Text style={{
+                                color: theme.textColor, fontSize: 16,
+                                marginRight: 16, fontWeight: '600',
+                                marginBottom: 3
+                            }}
+                                ellipsizeMode="tail"
+                                numberOfLines={1}
+                            >
+                                {t('products.staking.title')}
+                            </Text>
+                            <Text style={{
+                                color: theme.price, fontSize: 13,
+                                fontWeight: '400',
+                            }}
+                                ellipsizeMode="tail"
+                            >
+                                {isTestnet ? t('products.staking.subtitle.devPromo') : t("products.staking.subtitle.join", { apy: apyWithFee ?? '8' })}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </TouchableHighlight>
+    );
+})
---
+++ app/components/staking/UnstakeBanner.tsx
@@ -0,0 +1,96 @@
+import React, { useLayoutEffect, useRef } from "react"
+import { StyleProp, View, ViewStyle, Text, Platform } from "react-native";
+import LottieView from 'lottie-react-native';
+import { fromNano, toNano } from "@ton/core";
+import { formatNum } from "../../utils/numbers";
+import { t } from "../../i18n/t";
+import { formatCurrency } from "../../utils/formatCurrency";
+import { useTheme } from '../../engine/hooks';
+import { useNetwork } from '../../engine/hooks';
+import { usePrice } from '../../engine/hooks';
+
+export const UnstakeBanner = React.memo((
+    {
+        member,
+        style,
+        amount
+    }: {
+        member: {
+            balance: bigint,
+            pendingDeposit: bigint,
+            pendingWithdraw: bigint,
+            withdraw: bigint
+        },
+        style?: StyleProp<ViewStyle>,
+        amount?: string
+    }
+) => {
+    const theme = useTheme();
+    const { isTestnet } = useNetwork();
+    
+    const [price, currency] = usePrice();
+    const anim = useRef<LottieView>(null);
+
+    useLayoutEffect(() => {
+        if (Platform.OS === 'ios') {
+            setTimeout(() => {
+                anim.current?.play()
+            }, 300);
+        }
+    }, []);
+
+    const validAmount = amount?.replace(',', '.') || '0';
+    const value = React.useMemo(() => {
+        try {
+            return toNano(validAmount);
+        } catch (error) {
+            return BigInt(0);
+        }
+    }, [validAmount]);
+    const estInc = parseFloat(fromNano(value)) * 0.1;
+    const estIncPrice = estInc * (price?.price?.usd ?? 0) * (price?.price.rates[currency] ?? 0);
+    const formattedInc = formatNum(estInc < 0.01 ? estInc.toFixed(6) : estInc.toFixed(2));
+    const formattedPrice = formatCurrency(estInc < 0.01 ? estIncPrice.toFixed(6) : estIncPrice.toFixed(2), currency)
+
+    return (
+        <View style={{
+            backgroundColor: theme.item,
+            borderRadius: 14,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 20,
+        }}>
+            <LottieView
+                ref={anim}
+                source={require('../../../assets/animations/money.json')}
+                autoPlay={true}
+                loop={true}
+                style={{ width: 94, height: 94, marginBottom: 16, maxWidth: 140, maxHeight: 140 }}
+            />
+            <Text style={{
+                color: theme.textColor,
+                fontSize: 16,
+                fontWeight: '600',
+                textAlign: 'center',
+                maxWidth: 240,
+                marginBottom: 10
+            }}>
+                {isTestnet
+                    ? t('products.staking.banner.estimatedEarningsDev')
+                    : t('products.staking.banner.estimatedEarnings',
+                        {
+                            amount: formattedInc,
+                            price: formattedPrice,
+                        }
+                    )}
+            </Text>
+            <Text style={{
+                color: theme.label,
+                fontSize: 16,
+                fontWeight: '400'
+            }}>
+                {t('products.staking.banner.message')}
+            </Text>
+        </View>
+    );
+})

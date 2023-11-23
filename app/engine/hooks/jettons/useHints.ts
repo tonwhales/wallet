95e7feba2ea03fd55d393ecbcebcc9e03fd7da34
vzhovnitsky <vzhovnitsky@gmail.com>
2023-11-23 14:49:02 +0200

    wip: fixing hints fetch

--- app/engine/hooks/jettons/useHints.ts
+++ app/engine/hooks/jettons/useHints.ts
@@ -1,14 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { Queries } from '../../queries';
 import { fetchHints } from '../../api/fetchHints';
-import { Address } from '@ton/core';
-import { useMemo } from 'react';
 
 export function useHints(addressString?: string): string[] {
     let hints = useQuery({
         queryKey: Queries.Hints(addressString || ''),
         queryFn: async () => {
-            return await fetchHints(addressString!);
+            return (await fetchHints(addressString!)).hints;
         },
         enabled: !!addressString,
         refetchInterval: 10000,

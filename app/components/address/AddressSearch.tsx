d4d53bd7e684115d146d2d38c23e8e8fe46c4af3
vzhovnitsky <vzhovnitsky@gmail.com>
2023-11-29 18:53:46 +0200

    fix: own labeling in recent

--- app/components/address/AddressSearch.tsx
+++ app/components/address/AddressSearch.tsx
@@ -182,8 +182,24 @@ export const AddressSearch = memo(({
                         {filtered.recent.map((address, index) => {
                             const contact = contacts[address.toString({ testOnly: network.isTestnet })];
                             const known = knownWallets[address.toString({ testOnly: network.isTestnet })];
-                            const type = contact ? 'contact' : known ? 'known' : 'unknown';
-                            const title = contact ? contact.name : known ? known.name : t('contacts.unknown');
+                            const ownIndex = myWallets.findIndex((acc) => acc.address.equals(address));
+                            let type: "known" | "unknown" | "contact" | "my-wallets" = 'unknown';
+                            let title = t('contacts.unknown');
+                            if (contact) {
+                                type = 'contact';
+                                title = contact.name;
+                            } else if (known) {
+                                type = 'known';
+                                title = known.name;
+                            } else if (ownIndex >= 0) {
+                                type = 'my-wallets';
+                                const settings = walletsSettings[address.toString({ testOnly: network.isTestnet })];
+                                if (settings?.name) {
+                                    title = settings.name;
+                                } else {
+                                    title = `${t('common.wallet')} ${ownIndex + 1}`;
+                                }
+                            }
 
                             return (
                                 <AddressSearchItemView

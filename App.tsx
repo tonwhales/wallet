baaf624a7b57ff9a86cf178ed942b457ac544594
vzhovnitsky <vzhovnitsky@gmail.com>
2023-12-04 17:47:09 +0200

    fix/chore: enable screens & lib versions upgrade

--- App.tsx
+++ App.tsx
@@ -24,6 +24,9 @@ import * as SplashScreen from 'expo-splash-screen';
 import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { getThemeStyleState } from './app/engine/state/theme';
 import * as Sentry from '@sentry/react-native';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
 
 if (!__DEV__) {
   Sentry.init({

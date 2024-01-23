710a3dc58e4116028e13c71bb0f90706a9a148e1
vzhovnitsky <vzhovnitsky@gmail.com>
2024-01-23 15:32:20 +0200

    wip: fixing dev webview ton-x

--- app/fragments/dev/DevDAppWebviewFragment.tsx
+++ app/fragments/dev/DevDAppWebviewFragment.tsx
@@ -1,7 +1,7 @@
-import { Linking, Pressable, View } from "react-native";
+import { Linking, Platform, Pressable, Share, View } from "react-native";
 import { fragment } from "../../fragment";
 import { StatusBar } from "expo-status-bar";
-import { useDAppBridge, useNetwork, usePrice, useTheme } from "../../engine/hooks";
+import { useCreateDomainKeyIfNeeded, useDAppBridge, useNetwork, usePrice, useTheme } from "../../engine/hooks";
 import { useTypedNavigation } from "../../utils/useTypedNavigation";
 import { usePermissions } from "expo-notifications";
 import { useCallback, useEffect, useMemo, useState } from "react";
@@ -21,15 +21,23 @@ import SegmentedControl from "@react-native-segmented-control/segmented-control"
 import { ItemSwitch } from "../../components/Item";
 import { ATextInput } from "../../components/ATextInput";
 import { RoundButton } from "../../components/RoundButton";
+import { Ionicons } from '@expo/vector-icons';
+import { ScrollView } from "react-native-gesture-handler";
 
 import Chevron from '@assets/ic-chevron-down.svg';
+import { useKeysAuth } from "../../components/secure/AuthWalletKeys";
+import { getDomainKey } from "../../engine/state/domainKeys";
+import { ItemButton } from "../../components/ItemButton";
+import { t } from "../../i18n/t";
 
 const engineOptions: ('ton-x' | 'ton-connect' | 'none')[] = ['ton-x', 'ton-connect', 'none'];
 
 export const DevDAppWebviewFragment = fragment(() => {
+    const authContext = useKeysAuth();
     const safeArea = useSafeAreaInsets();
     const isTestnet = useNetwork().isTestnet;
     const navigation = useTypedNavigation();
+    const createDomainKeyIfNeeded = useCreateDomainKeyIfNeeded();
     const theme = useTheme();
     const [pushPemissions,] = usePermissions();
     const [, currency] = usePrice();
@@ -70,8 +78,6 @@ export const DevDAppWebviewFragment = fragment(() => {
     const linkNavigator = useLinkNavigator(isTestnet);
     const loadWithRequest = useCallback((event: ShouldStartLoadRequest): boolean => {
         try {
-            console.log(event.url);
-            console.log(endpoint);
             new URL(event.url);
             new URL(endpoint);
         } catch (error) {
@@ -102,7 +108,18 @@ export const DevDAppWebviewFragment = fragment(() => {
     //
     // Injection
     //
-    const domain = useMemo(() => extractDomain(endpoint), []);
+    const domain = useMemo(() => {
+        try {
+            return extractDomain(endpoint)
+        } catch {
+            return '';
+        }
+    }, []);
+    const [hasDomainKey, setHasDomainKey] = useState(getDomainKey(domain) !== undefined);
+
+    useEffect(() => {
+        setHasDomainKey(getDomainKey(domain) !== undefined);
+    }, [domain]);
 
     // ton-connect
     const { ref: webViewRef, isConnected, disconnect, ...tonConnectWebViewProps } = useDAppBridge(endpoint, navigation);
@@ -144,6 +161,14 @@ export const DevDAppWebviewFragment = fragment(() => {
     useEffect(() => {
         setRenderKey(renderKey + 1);
     }, [webViewProps.injectedJavaScriptBeforeContentLoaded, useMainButton, useStatusBar, useToaster, url]);
+
+    const onShare = useCallback(() => {
+        if (Platform.OS === 'ios') {
+            Share.share({ title: t('receive.share.title'), url: endpoint });
+        } else {
+            Share.share({ title: t('receive.share.title'), message: endpoint });
+        }
+    }, [endpoint]);
 
     return (
         <View style={{ flexGrow: 1 }}>
@@ -193,53 +218,106 @@ export const DevDAppWebviewFragment = fragment(() => {
                         justifyContent: 'center',
                         flexShrink: 1
                     }}>
-                        <SegmentedControl
-                            values={engineOptions}
-                            selectedIndex={engineOptions.indexOf(engine ?? 'none')}
-                            appearance={theme.style === 'dark' ? 'dark' : 'light'}
-                            onChange={(event) => setEngine(engineOptions[event.nativeEvent.selectedSegmentIndex] ?? null)}
-                            style={{ marginHorizontal: 16 }}
-                            backgroundColor={theme.surfaceOnBg}
-                            fontStyle={{ fontSize: 15, fontWeight: '500', color: theme.textPrimary }}
-                            activeFontStyle={{ fontSize: 15, fontWeight: '500', color: theme.textPrimary }}
-                        />
-                        <ItemSwitch
-                            title={'Main button API'}
-                            value={useMainButton}
-                            onChange={setUseMainButton}
-                        />
-                        <ItemSwitch
-                            title={'Status bar API'}
-                            value={useStatusBar}
-                            onChange={setUseStatusBar}
-                        />
-                        <ItemSwitch
-                            title={'Toaster API'}
-                            value={useToaster}
-                            onChange={setUseToaster}
-                        />
-                        <View style={{
-                            backgroundColor: theme.surfaceOnElevation,
-                            paddingVertical: 20,
-                            borderRadius: 20,
-                            marginHorizontal: 16,
-                            marginBottom: 16
-                        }}>
-                            <ATextInput
-                                style={{ paddingHorizontal: 16, flexShrink: 1 }}
-                                value={urlInput}
-                                onValueChange={setUrlInput}
-                                label="URL"
+                        <ScrollView>
+                            <View style={{
+                                flexDirection: 'row',
+                                marginBottom: 16, marginHorizontal: 16,
+                                paddingHorizontal: 8, paddingVertical: 4,
+                                justifyContent: 'flex-end',
+                                backgroundColor: theme.surfaceOnBg,
+                                borderRadius: 8,
+                                gap: 8
+                            }}>
+                                <Pressable
+                                    style={{ padding: 8, justifyContent: 'center', alignItems: 'center' }}
+                                    onPress={onShare}
+                                >
+                                    <Ionicons
+                                        name={'share'}
+                                        size={24}
+                                        color={theme.iconNav}
+                                    />
+                                </Pressable>
+                                <Pressable
+                                    style={{ padding: 8, justifyContent: 'center', alignItems: 'center' }}
+                                    onPress={webViewRef.current?.reload}
+                                >
+                                    <Ionicons
+                                        name={'refresh'}
+                                        size={24}
+                                        color={theme.iconNav}
+                                    />
+                                </Pressable>
+                            </View>
+                            <SegmentedControl
+                                values={engineOptions}
+                                selectedIndex={engineOptions.indexOf(engine ?? 'none')}
+                                appearance={theme.style === 'dark' ? 'dark' : 'light'}
+                                onChange={(event) => setEngine(engineOptions[event.nativeEvent.selectedSegmentIndex] ?? null)}
+                                style={{ marginHorizontal: 16 }}
+                                backgroundColor={theme.surfaceOnBg}
+                                fontStyle={{ fontSize: 15, fontWeight: '500', color: theme.textPrimary }}
+                                activeFontStyle={{ fontSize: 15, fontWeight: '500', color: theme.textPrimary }}
                             />
-                        </View>
-                        <RoundButton
-                            title={'Save'}
-                            style={{ marginHorizontal: 16, marginBottom: 16 }}
-                            onPress={() => {
-                                setModalVisible(false);
-                                setUrl(urlInput);
-                            }}
-                        />
+                            {engine === 'ton-x' && (
+                                <ItemButton
+                                    title={`${hasDomainKey ? '' : 'Create'} Domain key`}
+                                    hint={hasDomainKey ? 'Exists' : 'Not domain key'}
+                                    onPress={!hasDomainKey ? async () => {
+                                        let domain = extractDomain(endpoint);
+                                        let created = await createDomainKeyIfNeeded(
+                                            domain,
+                                            authContext,
+                                            undefined,
+                                            {
+                                                backgroundColor: theme.elevation,
+                                                containerStyle: { paddingBottom: safeArea.bottom + 56 },
+                                            },
+                                        );
+                                        if (created) {
+                                            setHasDomainKey(true);
+                                        }
+                                    } : undefined}
+                                />
+                            )}
+                            <ItemSwitch
+                                title={'Main button API'}
+                                value={useMainButton}
+                                onChange={setUseMainButton}
+                            />
+                            <ItemSwitch
+                                title={'Status bar API'}
+                                value={useStatusBar}
+                                onChange={setUseStatusBar}
+                            />
+                            <ItemSwitch
+                                title={'Toaster API'}
+                                value={useToaster}
+                                onChange={setUseToaster}
+                            />
+                            <View style={{
+                                backgroundColor: theme.surfaceOnElevation,
+                                paddingVertical: 20,
+                                borderRadius: 20,
+                                marginHorizontal: 16,
+                                marginBottom: 16
+                            }}>
+                                <ATextInput
+                                    style={{ paddingHorizontal: 16, flexShrink: 1 }}
+                                    value={urlInput}
+                                    onValueChange={setUrlInput}
+                                    label="URL"
+                                />
+                            </View>
+                            <RoundButton
+                                title={'Save'}
+                                style={{ marginHorizontal: 16, marginBottom: 16 }}
+                                onPress={() => {
+                                    setModalVisible(false);
+                                    setUrl(urlInput);
+                                }}
+                            />
+                        </ScrollView>
                     </View>
                 </View>
             </Modal>

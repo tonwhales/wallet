de2242577f530163ddc4fcdad141622dac81456f
vzhovnitsky <vzhovnitsky@gmail.com>
2024-01-23 16:44:07 +0200

    cleanup: rm redundant track prop

--- app/systemFragment.tsx
+++ app/systemFragment.tsx
@@ -1,24 +1,10 @@
 import * as React from 'react';
 import { GlobalLoaderProvider } from './components/useGlobalLoader';
-import { useTrackScreen } from './analytics/mixpanel';
-import { useRoute } from '@react-navigation/native';
 import { AuthWalletKeysContextProvider } from './components/secure/AuthWalletKeys';
-import { useNetwork } from './engine/hooks';
 import { ToastProvider } from './components/toast/ToastProvider';
 
-export function systemFragment<T>(
-    Component: React.ComponentType<T>,
-    track?: boolean
-): React.ComponentType<React.PropsWithRef<T & JSX.IntrinsicAttributes>> {
+export function systemFragment<T>(Component: React.ComponentType<T>): React.ComponentType<React.PropsWithRef<T & JSX.IntrinsicAttributes>> {
     return React.memo((props: T & JSX.IntrinsicAttributes) => {
-        const { isTestnet } = useNetwork();
-
-        const route = useRoute();
-        const name = route.name;
-        if (track) {
-            useTrackScreen(name, isTestnet);
-        }
-
         return (
             <AuthWalletKeysContextProvider>
                 <GlobalLoaderProvider>

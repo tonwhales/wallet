de9a1b2e83a5eea93b6a26291165a69afe96e625
Vladislav <32094306+vzhovnitsky@users.noreply.github.com>
2024-01-25 12:56:36 +0200

    fix: fixing banners query key (#736)

--- app/engine/queries.ts
+++ app/engine/queries.ts
@@ -47,5 +47,5 @@ export const Queries = {
     }),
     APY: (network: 'mainnet' | 'testnet') => (['staking', 'apy', network]),
 
-    Banners: (language: string) => (['banners', language]),
+    Banners: (language: string, version: string, buildNumber: string) => (['banners', language, version, buildNumber]),
 }

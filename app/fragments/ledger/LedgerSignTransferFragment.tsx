635d0671c78641e5f6e7c29f971f14b51a26b13b
vzhovnitsky <vzhovnitsky@gmail.com>
2024-03-14 15:05:29 +0200

    fix: props

--- app/fragments/ledger/LedgerSignTransferFragment.tsx
+++ app/fragments/ledger/LedgerSignTransferFragment.tsx
@@ -263,6 +263,7 @@ const LedgerTransferLoaded = memo((props: ConfirmLoadedProps & ({ setTransferSta
                 contentContainerStyle={{ paddingBottom: 56 }}
             >
                 <TransferSingleView
+                    metadata={metadata}
                     operation={operation}
                     order={order}
                     amount={order.amountAll ? (account?.balance ?? 0n) : order.amount}

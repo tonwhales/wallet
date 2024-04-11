de2a28ccd86ee08bc8c36d8a521670b14c98bd4e
vzhovnitsky <vzhovnitsky@gmail.com>
2024-04-11 16:17:51 +0300

    fix: cleanup countryCode lookup

--- app/modules/StoreFront.ts
+++ app/modules/StoreFront.ts
@@ -4,7 +4,7 @@ const { StoreFront } = NativeModules;
 
 export function getStoreFront(): string | null {
     return Platform.select({
-        ios: StoreFront.getConstants().countryCode as string | null,
+        ios: (StoreFront?.getConstants()?.countryCode ?? null) as string | null,
         android: null,
         default: null
     });

97d0ef347e375d0cbd67c137e40462fdc8b34308
vzhovnitsky <vzhovnitsky@gmail.com>
2024-04-17 18:17:52 +0300

    cleanup: rm dev logs

--- app/engine/hooks/useAppVisible.ts
+++ app/engine/hooks/useAppVisible.ts
@@ -2,21 +2,14 @@ import { useEffect, useRef, useState } from "react";
 import { AppState } from "react-native";
 
 export function useAppVisible() {
+    // persist the current app state across re-renders and to provide a mutable reference
     const appState = useRef(AppState.currentState);
     const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
     useEffect(() => {
         const subscription = AppState.addEventListener('change', nextAppState => {
-            if (
-                appState.current.match(/inactive|background/) &&
-                nextAppState === 'active'
-            ) {
-                console.log('App has come to the foreground!');
-            }
-
             appState.current = nextAppState;
             setAppStateVisible(appState.current);
-            console.log('AppState', appState.current);
         });
 
         return () => {

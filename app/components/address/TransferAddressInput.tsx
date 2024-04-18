410c1521e8d52f478b7312570f347ce5316f3989
vzhovnitsky <vzhovnitsky@gmail.com>
2024-04-18 16:13:01 +0300

    Merge branch 'develop' into perf/hub-1047
    # Conflicts:
    #	app/components/address/TransferAddressInput.tsx

--- app/components/address/TransferAddressInput.tsx
+++ app/components/address/TransferAddressInput.tsx
@@ -13,10 +13,10 @@ import { t } from "../../i18n/t";
 import { PerfText } from "../basic/PerfText";
 import { avatarHash } from "../../utils/avatarHash";
 import { useLedgerTransport } from "../../fragments/ledger/components/TransportContext";
-import { useAddressBookContext } from "../../engine/AddressBookContext";
+import { AddressInputAvatar } from "./AddressInputAvatar";
+import { useDimensions } from "@react-native-community/hooks";
 
 import IcChevron from '@assets/ic_chevron_forward.svg';
-import { AddressInputAvatar } from "./AddressInputAvatar";
 
 type TransferAddressInputProps = {
     acc: Address,
@@ -132,6 +132,8 @@ export const TransferAddressInput = memo(forwardRef((props: TransferAddressInput
     const contact = addressBookContext.asContact(props.target);
     const appState = useAppState();
     const theme = useTheme();
+    const dimentions = useDimensions();
+    const screenWidth = dimentions.screen.width;
     const validAddressFriendly = props.validAddress?.toString({ testOnly: props.isTestnet });
     const [walletSettings,] = useWalletSettings(validAddressFriendly);
     const ledgerTransport = useLedgerTransport();
@@ -171,17 +173,18 @@ export const TransferAddressInput = memo(forwardRef((props: TransferAddressInput
         }
     });
 
+    const isSelected = props.isSelected;
+
     const select = useCallback(() => {
         (ref as RefObject<ATextInputRef>)?.current?.focus();
-    }, [ref]);
+    }, []);
 
     useEffect(() => {
-        if (props.isSelected) {
+        if (isSelected) {
             select();
         }
-    }, [select]);
+    }, [select, isSelected]);
 
-    const isSelected = props.isSelected;
 
     return (
         <View>
@@ -232,44 +235,50 @@ export const TransferAddressInput = memo(forwardRef((props: TransferAddressInput
                 </Pressable>
             </View>
             <View
-                style={[!isSelected ? [{ opacity: 0, height: 0 }, Platform.select({ ios: { width: 0 } })] : {}]}
+                style={
+                    !isSelected
+                        ? Platform.select({
+                            ios: { width: 0, height: 0, opacity: 0 },
+                            android: { height: 1, opacity: 0 } // to account for wierd android behavior (not focusing on input when it's height/width is 0)
+                        })
+                        : { height: 'auto', width: '100%', opacity: 1 }
+                }
                 pointerEvents={isSelected ? undefined : 'none'}
             >
                 <View style={{
                     backgroundColor: props.theme.surfaceOnElevation,
                     paddingVertical: 20,
+                    paddingHorizontal: 20,
                     width: '100%', borderRadius: 20,
-                    flexDirection: 'row', alignItems: 'center'
+                    flexDirection: 'row', alignItems: 'center',
+                    gap: 16
                 }}>
-                    <View style={{ marginLeft: 20 }}>
-                        <AddressInputAvatar
-                            size={46}
-                            theme={theme}
-                            isTestnet={props.isTestnet}
-                            isOwn={own}
-                            markContact={!!contact}
-                            hash={walletSettings?.avatar}
-                            isLedger={isSelectedLedger}
-                            friendly={validAddressFriendly}
-                            avatarColor={avatarColor}
-                        />
-                    </View>
-                    <View style={{ flexGrow: 1 }}>
-                        <AddressDomainInput
-                            input={props.input}
-                            dispatch={props.dispatch}
-                            target={props.target}
-                            index={props.index}
-                            ref={ref}
-                            autoFocus={true}
-                            onFocus={props.onFocus}
-                            isKnown={isKnown}
-                            onSubmit={props.onSubmit}
-                            contact={contact}
-                            onQRCodeRead={props.onQRCodeRead}
-                            domain={props.domain}
-                        />
-                    </View>
+                    <AddressInputAvatar
+                        size={46}
+                        theme={theme}
+                        isTestnet={props.isTestnet}
+                        isOwn={own}
+                        markContact={!!contact}
+                        hash={walletSettings?.avatar}
+                        isLedger={isSelectedLedger}
+                        friendly={validAddressFriendly}
+                        avatarColor={avatarColor}
+                    />
+                    <AddressDomainInput
+                        input={props.input}
+                        dispatch={props.dispatch}
+                        target={props.target}
+                        index={props.index}
+                        ref={ref}
+                        autoFocus={true}
+                        onFocus={props.onFocus}
+                        isKnown={isKnown}
+                        onSubmit={props.onSubmit}
+                        contact={contact}
+                        onQRCodeRead={props.onQRCodeRead}
+                        domain={props.domain}
+                        screenWidth={screenWidth * 0.75}
+                    />
                 </View>
                 {!props.validAddress && (props.target.length >= 48) && (
                     <Animated.View entering={FadeIn} exiting={FadeOut}>
@@ -297,7 +306,7 @@ export const TransferAddressInput = memo(forwardRef((props: TransferAddressInput
 
                         props.dispatch({
                             type: InputActionType.InputTarget,
-                            input: name,
+                            input: name.trim(),
                             target: friendly
                         });
 

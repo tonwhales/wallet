4673494be310424c53eaf2f888e16970a17535dd
vzhovnitsky <vzhovnitsky@gmail.com>
2024-05-03 10:53:06 +0300

    fix: indexer url

--- app/engine/api/fetchPoolApy.ts
+++ app/engine/api/fetchPoolApy.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { z } from 'zod';
+import { stakingIndexerUrl } from "./fetchStakingNominator";
 
 const poolApyResCodec = z.array(z.object({
     poolApy: z.string(),
@@ -11,11 +12,9 @@ export type PoolApy = z.infer<typeof poolApyResCodec>;
 
 export async function fetchPoolApy(address: string) {
     const res = (await axios.post(
-        `https://staking-indexer-shci7.ondigitalocean.app/indexer/pool/apy`,
+        `${stakingIndexerUrl}/indexer/pool/apy`,
         { address, fixedPeriod: 'week' }
     )).data;
-
-    console.log(`[Pool apy] for ${address} response: `, res);
 
     const parsed = poolApyResCodec.safeParse(res);
 

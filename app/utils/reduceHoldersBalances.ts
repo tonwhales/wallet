b86f081f9f8d2057e0645d21ea437a67538d2a0e
vzhovnitsky <vzhovnitsky@gmail.com>
2024-06-13 17:17:53 +0300

    fix: devision by zero

--- app/utils/reduceHoldersBalances.ts
+++ app/utils/reduceHoldersBalances.ts
@@ -4,12 +4,13 @@ import { toBnWithDecimals } from "./withDecimals";
 
 export function reduceHoldersBalances(accs: GeneralHoldersAccount[], priceUSD: number) {
     return accs.reduce((acc, item) => {
-        if (!item || !item.cryptoCurrency || !item.balance) return acc;
-        
+        if (!item || !item.cryptoCurrency || !item.balance || !(item.balance === '0')) return acc;
+
         if (item.cryptoCurrency.ticker === 'TON') {
             return acc + BigInt(item.balance);
         }
-        const amount = toBnWithDecimals(item.balance, item.cryptoCurrency.decimals) / toNano(priceUSD ?? 1);
+
+        const amount = toBnWithDecimals(item.balance, item.cryptoCurrency.decimals) / toNano(priceUSD || 1);
         return acc + toBnWithDecimals(amount, item.cryptoCurrency.decimals);
     }, BigInt(0));
 }

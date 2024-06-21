e92e276d7f100dc1fd2df2c1088617cfa48ae1e9
vzhovnitsky <vzhovnitsky@gmail.com>
2024-06-21 14:40:10 +0300

    fix: fixing injected dApp removing connections

--- app/engine/hooks/dapps/useRemoveInjectedConnection.ts
+++ app/engine/hooks/dapps/useRemoveInjectedConnection.ts
@@ -1,4 +1,5 @@
 import { getCurrentAddress } from "../../../storage/appState";
+import { warn } from "../../../utils/log";
 import { useConnectExtensions } from "../../hooks/dapps/useTonConnectExtenstions";
 import { TonConnectBridgeType } from '../../tonconnect/types';
 import { extensionKey } from "./useAddExtension";
@@ -9,9 +10,18 @@ export function useRemoveInjectedConnection() {
     const setConnections = useSetAppsConnectionsState();
 
     return (endpoint: string) => {
+        // format endpoint to origin
+        try {
+            const url = new URL(endpoint);
+            endpoint = url.origin;
+        } catch {
+            warn(`Invalid URL ${endpoint}`);
+            return;
+        }
         let key = extensionKey(endpoint);
 
         const app = extensions[key];
+        
         if (!app) {
             return;
         }
@@ -21,8 +31,11 @@ export function useRemoveInjectedConnection() {
         setConnections(
             currentAccount.addressString,
             (prev) => {
-                prev[key] = (prev[key] ?? []).filter((item) => item.type !== TonConnectBridgeType.Injected);
-                return prev;
+                const newConnections = (prev[key] ?? []).filter((item) => item.type !== TonConnectBridgeType.Injected);
+                return {
+                    ...prev,
+                    [key]: newConnections,
+                };
             }
         );
     }

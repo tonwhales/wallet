8d28c1dd4f09fd54c4e6168aee8fbda1f56032b7
vzhovnitsky <vzhovnitsky@gmail.com>
2024-08-08 01:26:21 +0300

    wip: adding ui extension

--- ios/WUIExt/WUIExtView.swift
+++ ios/WUIExt/WUIExtView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 import PassKit
 
+// struct WUIExtView: View {
+struct PasscodeDotView: View {
+  @Binding var isSelected: Bool
+  
+  @State private var scale: CGFloat = 1.0
+  
+  var body: some View {
+    Circle()
+      .fill(isSelected ? Color.accentColor : Color.gray)
+      .frame(width: 10, height: 10)
+      .scaleEffect(scale)
+      .onChange(of: isSelected) { oldValue, newValue in
+        if newValue {
+          withAnimation(.easeInOut(duration: 0.2)) {
+            scale = 1.4
+          }
+          withAnimation(.easeInOut(duration: 0.2).delay(0.2)) {
+            scale = 1.0
+          }
+        }
+      }
+  }
+}
+
 struct WUIExtView: View {
   var completionHandler: ((PKIssuerProvisioningExtensionAuthorizationResult) -> Void)?
+  @State private var passcode: String = ""
+  
+  static let accent = Color(red: 86 / 255, green: 76 / 255, blue: 226 / 255)
+  static let accentDark = Color(red: 94 / 255, green: 84 / 255, blue: 242 / 255)
   
   func handleLogin() -> Void {
     // Create username/password login logic.
     // You can copy and reuse the username/password
     // login logic from the containing issuer app.
-    print("Log In button tapped")
     let randomNum = Int.random(in: 1..<10)
     let authorized = randomNum > 5 ? true : false
     
@@ -44,63 +71,83 @@ struct WUIExtView: View {
   }
   
   var body: some View {
-    // SwiftUi screen containing a title, description, entered passcode dots, pin pad buttons and a Face ID button
     VStack {
-      Text("Log In")
+      Spacer()
+      titleView
+      passcodeDotsView
+      Spacer()
+      pinPadView
+    }
+  }
+  
+  private var titleView: some View {
+    Text("Enter your PIN code")
+      .font(.title)
+      .padding()
+  }
+  
+  private var passcodeDotsView: some View {
+    HStack {
+      ForEach(0..<6) { index in
+        PasscodeDotView(isSelected: .constant(index < passcode.count))
+      }
+    }
+  }
+  
+  private func createButton(row: Int, column: Int) -> some View {
+    Button(action: {
+      if (passcode.count < 6) {
+        passcode += "\(row * 3 + column + 1)"
+        print("Button \(row * 3 + column + 1) tapped")
+      }
+    }) {
+      Text("\(row * 3 + column + 1)")
+        .font(.title)
+        .foregroundStyle(.black)
+        .padding()
+    }
+  }
+  
+  private func createRow(row: Int) -> some View {
+    HStack(spacing: 60) {
+      ForEach(0..<3) { column in
+        createButton(row: row, column: column)
+      }
+    }
+  }
+  
+  private func deleteButton() -> some View {
+    Button(action: {
+      if passcode.count > 0 {
+        passcode.removeLast()
+      }
+      print("Delete button tapped")
+    }) {
+      Image(systemName: "delete.left")
+        .foregroundStyle(.gray)
         .font(.title)
         .padding()
-      
-      Text("Enter your passcode")
-        .font(.subheadline)
+    }
+  }
+  
+  private func faceIdButton() -> some View {
+    Button(action: handleBiometricLogin) {
+      Image(systemName: "faceid")
+        .font(.title)
+        .foregroundStyle(.gray)
         .padding()
-      
-      HStack {
-        ForEach(0..<6) { index in
-          Circle()
-            .frame(width: 10, height: 10)
-            .foregroundColor(.black)
-            .padding()
-        }
+    }
+  }
+  
+  private var pinPadView: some View {
+    VStack(spacing: 20) {
+      ForEach(0..<3) { row in
+        createRow(row: row)
       }
-      
-      VStack {
-        ForEach(0..<3) { row in
-          HStack {
-            ForEach(0..<3) { column in
-              Button(action: {
-                print("Button \(row * 3 + column + 1) tapped")
-              }) {
-                Text("\(row * 3 + column + 1)")
-                  .font(.title)
-                  .padding()
-              }
-            }
-          }
-        }
-        
-        HStack {
-          Button(action: {
-            print("Button 0 tapped")
-          }) {
-            Text("0")
-              .font(.title)
-              .padding()
-          }
-          
-          Button(action: {
-            print("Delete button tapped")
-          }) {
-            Image(systemName: "delete.left")
-              .font(.title)
-              .padding()
-          }
-        }
-      }
-      
-      Button(action: handleBiometricLogin) {
-        Text("Face ID")
-          .font(.title)
-          .padding()
+      HStack(spacing: 50) {
+        faceIdButton()
+        createButton(row: 0, column: -1)
+        deleteButton()
       }
     }
   }

1649c2c391909bf5446e6bc491ab7c373639784d
Garrethta <g@rrethta.xyz>
2024-09-09 01:06:59 +0400

    wip: temporary invite api route fallback

--- app/engine/api/holders/fetchAddressInviteCheck.ts
+++ app/engine/api/holders/fetchAddressInviteCheck.ts
@@ -4,24 +4,57 @@ import { z } from "zod";
 import { Address } from "@ton/core";
 
 const inviteCheckCodec = z.object({
-    allowed: z.boolean(),
+  allowed: z.boolean(),
 });
 
-export async function fetchAddressInviteCheck(address: string, isTestnet: boolean) {
-    const endpoint = holdersEndpoint(isTestnet);
-    const formattedAddress = Address.parse(address).toString({ testOnly: isTestnet });
+export async function fetchAddressInviteCheck(
+  address: string,
+  isTestnet: boolean
+) {
+  const endpoint = holdersEndpoint(isTestnet);
+  const formattedAddress = Address.parse(address).toString({
+    testOnly: isTestnet,
+  });
 
-    const res = await axios.post(`https://${endpoint}/v2/invite/wallet/check`, { 
-        wallet: formattedAddress, 
-        network: isTestnet ? 'ton-testnet' : 'ton-mainnet'
+  try {
+    let res = await axios.post(`https://${endpoint}/v2/invite/wallet/check`, {
+      wallet: formattedAddress,
+      network: isTestnet ? "ton-testnet" : "ton-mainnet",
     });
+
+    if (res.status >= 400) {
+      res = await axios.post(`https://${endpoint}/v2/whitelist/wallet/check`, {
+        wallet: formattedAddress,
+      });
+    }
 
     const parsed = inviteCheckCodec.safeParse(res.data);
 
     if (!parsed.success) {
-        console.warn('Failed to parse invite check response', parsed.error);
-        return false;
+      console.warn("Failed to parse invite check response", parsed.error);
+      return false;
     }
 
     return parsed.data.allowed;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      if (error.response!.status >= 400) {
+        const res = await axios.post(
+          `https://${endpoint}/v2/whitelist/wallet/check`,
+          {
+            wallet: formattedAddress,
+          }
+        );
+
+        const parsed = inviteCheckCodec.safeParse(res.data);
+
+        if (!parsed.success) {
+          console.warn("Failed to parse invite check response", parsed.error);
+          return false;
+        }
+
+        return parsed.data.allowed;
+      }
+    }
+  }
 }

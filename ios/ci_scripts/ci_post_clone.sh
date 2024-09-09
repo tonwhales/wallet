fdbdcb46bf1ce1dcdfb8b66013669f274f4a8af4
vzhovnitsky <vzhovnitsky@gmail.com>
2024-09-09 12:20:54 +0200

    wip: fixing rexml versioning

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -53,6 +53,7 @@ gem uninstall rexml -v '3.3.6' || true
 gem install rexml -v '3.2.4'
 
 echo "===== Installing pods ====="
-pod install
+bundle exec pod install
+
 # the sed command from RN cant find the file... so we have to run it ourselves
 sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h

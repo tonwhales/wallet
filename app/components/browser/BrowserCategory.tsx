63bd8891ffbafb4db615c85a63cd938fc69dedee
Alexandr Kulinkovich <alexandr_kulinkovich@icloud.com>
2024-09-23 11:21:05 +0300

    Fix horizontal scroll

--- app/components/browser/BrowserCategory.tsx
+++ app/components/browser/BrowserCategory.tsx
@@ -1,23 +1,31 @@
 import { memo, useMemo } from "react";
-import { ListingsCategory } from "./BrowserListings";
-import { View, Text, Platform } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { View, Text, Platform, Dimensions, StyleSheet } from "react-native";
+
+import { BrowserListingItem, ListingsCategory } from "./BrowserListings";
 import { BrowserListItem } from "./BrowserListItem";
 import { TypedNavigation } from "../../utils/useTypedNavigation";
 import { ThemeType } from "../../engine/state/theme";
 import { Typography } from "../styles";
-import { FlatList } from "react-native-gesture-handler";
-import { useDimensions } from "@react-native-community/hooks";
 
-export const BrowserCategory = memo(({
-    category,
-    navigation,
-    theme
-}: {
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+interface BrowserCategoryProps {
     category: ListingsCategory,
     navigation: TypedNavigation,
     theme: ThemeType
-}) => {
-    const dimensions = useDimensions();
+}
+
+interface ColumnItemProps {
+    index: number
+    columns: BrowserListingItem[]
+    navigation: TypedNavigation
+    theme: ThemeType
+}
+
+export const BrowserCategory = memo((props: BrowserCategoryProps) => {
+    const { category, navigation, theme } = props
+
     const columns = useMemo(() => {
         const sorted = category.listings.sort((a, b) => {
             const wA = a.weight ?? 0;
@@ -42,17 +50,20 @@ export const BrowserCategory = memo(({
         return columns;
     }, [category.listings]);
 
+
     return (
-        <View style={{ marginBottom: 16 }}>
+        <View style={styles.container}>
             <Text style={[
-                { color: theme.textPrimary, marginHorizontal: 16 },
+                { color: theme.textPrimary },
+                styles.title,
                 Typography.semiBold20_28
             ]}>
                 {category.title}
             </Text>
             {category.description && (
                 <Text style={[
-                    { color: theme.textSecondary, marginHorizontal: 16, marginBottom: 16 },
+                    { color: theme.textSecondary },
+                    styles.description,
                     Typography.regular15_20
                 ]}>
                     {category.description}
@@ -60,45 +71,64 @@ export const BrowserCategory = memo(({
             )}
             <FlatList
                 data={columns}
-                keyExtractor={(item, index) => `col-${index}`}
+                keyExtractor={(_, index) => `col-${index}`}
                 horizontal
                 scrollEnabled={columns.length > 1}
-                style={{ maxHeight: (56 + 24) * 3, width: '100%' }}
                 showsHorizontalScrollIndicator={false}
-                snapToInterval={dimensions.screen.width - 16}
+                snapToInterval={SCREEN_WIDTH - 64}
                 decelerationRate={'fast'}
-                contentInset={{ left: 0, right: 32 }}
                 overScrollMode={'never'}
-                renderItem={({ item, index }) => (
-                    <View
-                        key={`category-col-${index}`}
-                        style={[
-                            {
-                                width: columns.length > 1 ? dimensions.screen.width - 64 : dimensions.screen.width,
-                                marginRight: 16
-                            },
-                            Platform.select({ android: { marginRight: index === columns.length - 1 ? 32 : 16 } })
-                        ]}
-                    >
-                        {item.map((item, index) => (
-                            <View
-                                key={`category-item-${index}`}
-                                style={[
-                                    { marginLeft: 16, marginBottom: 24, width: '100%' },
-                                    Platform.select({ android: { marginRight: index === 0 ? 16 : 0 } })
-                                ]}
-                            >
-                                <BrowserListItem
-                                    key={index}
-                                    item={item}
-                                    navigation={navigation}
-                                    theme={theme}
-                                />
-                            </View>
-                        ))}
-                    </View>
-                )}
+                renderItem={({ item, index }) =>
+                    <ColumnItem
+                        index={index}
+                        columns={item}
+                        navigation={navigation}
+                        theme={theme}
+                    />
+                }
             />
         </View>
     );
 });
+
+
+
+const ColumnItem = memo((props: ColumnItemProps) => {
+    const { index, theme, columns, navigation } = props
+
+    return <View
+        key={`category-col-${index}`}
+        style={[
+            styles.box,
+            Platform.select({ android: { marginRight: index === columns.length - 1 ? 32 : 16 } }),
+        ]}
+    >
+        {columns.map((item: any, index: number) => (
+            <View
+                key={`category-item-${index}`}
+                style={[
+                    styles.columnContainer,
+                    Platform.select({ android: { marginRight: index === 0 ? 16 : 0 } })
+                ]}
+            >
+                <BrowserListItem
+                    key={index}
+                    item={item}
+                    navigation={navigation}
+                    theme={theme}
+                />
+            </View>
+        ))}
+    </View>
+})
+
+const styles = StyleSheet.create({
+    container: { marginBottom: 16 },
+    title: { marginHorizontal: 16 },
+    description: { marginHorizontal: 16, marginBottom: 16 },
+    box: {
+        paddingHorizontal: 8,
+        width: SCREEN_WIDTH - 42,
+    },
+    columnContainer: { marginBottom: 24, width: '100%', }
+})

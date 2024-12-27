7106e4c913d8b3e4f3713c695d6cac33d2789e23
vzhovnitsky <vzhovnitsky@gmail.com>
2024-12-27 01:57:43 +0100

    feat: new holders address searchable item view

--- app/components/address/HoldersAccountsSearch.tsx
+++ app/components/address/HoldersAccountsSearch.tsx
@@ -1,60 +1,70 @@
 import React from "react";
 import { memo, useMemo } from "react";
-import { AddressSearchItemView } from "./AddressSearchItemView";
 import { Platform, Text, View } from "react-native";
-import { useNetwork, useWalletsSettings } from "../../engine/hooks";
+import { useHoldersAccountStatus, useNetwork } from "../../engine/hooks";
 import { t } from "../../i18n/t";
 import { useDebouncedValue } from "../../utils/useDebouncedValue";
 import { ThemeType } from "../../engine/state/theme";
-import { HoldersAccountTarget } from "../../engine/hooks/holders/useHoldersAccountTrargets";
 import { Typography } from "../styles";
 import { getAccountName } from "../../utils/holders/getAccountName";
 import { AddressSearchItem } from "./AddressSearch";
+import { HoldersAccountItem } from "../products/HoldersAccountItem";
+import { GeneralHoldersAccount } from "../../engine/api/holders/fetchAccounts";
+import { Address } from "@ton/core";
+
+type HoldersSearchItem = AddressSearchItem & { acc: GeneralHoldersAccount };
 
 export const HoldersAccountsSearch = memo(({
     query,
     onSelect,
-    transfer,
     theme,
-    holdersAccounts
+    holdersAccounts,
+    owner,
 }: {
     query?: string,
     onSelect?: (item: AddressSearchItem) => void,
-    transfer?: boolean,
     theme: ThemeType,
-    holdersAccounts?: HoldersAccountTarget[]
+    holdersAccounts?: GeneralHoldersAccount[],
+    owner: Address,
 }) => {
-    const network = useNetwork();
-    const [walletsSettings] = useWalletsSettings();
+    const { isTestnet } = useNetwork();
     const debouncedQuery = useDebouncedValue(query?.toLowerCase(), 500);
+    const holdersAccStatus = useHoldersAccountStatus(owner).data;
 
-    const searchItems = useMemo(() => {
-        return {
-            holders: (holdersAccounts || []).map((acc) => {
+    const searchItems: HoldersSearchItem[] = useMemo(() => {
+        return (holdersAccounts || [])
+            .filter((a) => !!a.address)
+            .map((acc) => {
                 const title = getAccountName(acc.accountIndex, acc.name);
-                const searchable = `${title.toLowerCase()} ${acc.address.toString({ testOnly: network.isTestnet }).toLowerCase()}`;
+                const address = Address.parse(acc.address!!);
+                const searchable = `${title.toLowerCase()} ${acc.address!.toLowerCase()}`;
+                let memo: string | undefined = undefined;
+
+                if (acc.cryptoCurrency.ticker === 'TON') {
+                    memo = 'Top Up';
+                }
 
                 return {
                     type: 'holders',
                     addr: {
                         isBounceable: true,
-                        isTestOnly: network.isTestnet,
-                        address: acc.address
+                        isTestOnly: isTestnet,
+                        address
                     },
                     title,
                     searchable,
-                    memo: acc.memo
+                    memo,
+                    acc
                 }
-            }) as AddressSearchItem[],
-        };
-    }, [network, holdersAccounts]);
+            });
+    }, [isTestnet, holdersAccounts]);
 
     const filtered = useMemo(() => {
         if (!debouncedQuery || debouncedQuery.length === 0) {
-            return searchItems.holders;
+            return searchItems;
         }
 
-        return searchItems.holders.filter((i) => i.searchable.includes(debouncedQuery))
+        return searchItems.filter((i) => i.searchable.includes(debouncedQuery))
     }, [searchItems, debouncedQuery]);
 
     if (!filtered || filtered.length === 0) {
@@ -63,43 +73,41 @@ export const HoldersAccountsSearch = memo(({
 
     return (
         <View style={{ marginTop: 16 }}>
-            <View style={transfer ? {
+            <View style={{
                 borderRadius: 20,
-                backgroundColor: theme.surfaceOnElevation,
                 padding: 2
-            } : undefined}>
+            }}>
                 <Text style={[{
                     color: theme.textPrimary,
                     marginBottom: 12,
-                    marginLeft: transfer ? 16 : 0,
-                    marginTop: transfer ? 16 : 0
+                    marginTop: 8
                 }, Typography.semiBold17_24]}>
                     {t('products.holders.accounts.title')}
                 </Text>
-                <View style={transfer ? {
-                    backgroundColor: theme.elevation,
+                <View style={{
                     borderRadius: 18,
-                    paddingHorizontal: 16,
-                    paddingVertical: 8
-                } : undefined}>
+                    gap: 4
+                }}>
                     {filtered.map((item, index) => {
+                        const onOpen = () => onSelect?.(item);
                         return (
-                            <AddressSearchItemView
-                                key={index}
-                                item={item}
-                                onPress={onSelect}
-                                walletsSettings={walletsSettings}
-                                testOnly={network.isTestnet}
-                                theme={theme}
-                                knownWallets={{}}
+                            <HoldersAccountItem
+                                key={`holders-${index}`}
+                                owner={owner}
+                                account={item.acc}
+                                itemStyle={{ backgroundColor: theme.surfaceOnElevation }}
+                                style={{ paddingVertical: 0 }}
+                                isTestnet={isTestnet}
+                                hideCardsIfEmpty
+                                holdersAccStatus={holdersAccStatus}
+                                onOpen={onOpen}
+                                addressDescription
                             />
                         );
                     })}
                 </View>
             </View>
-            {Platform.OS === 'android' && (
-                <View style={{ height: 56 }} />
-            )}
+            {Platform.OS === 'android' && (<View style={{ height: 56 }} />)}
         </View>
     );
 });

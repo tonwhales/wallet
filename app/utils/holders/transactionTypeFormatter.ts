01e297a416b42116d215961bdac0fbbf6c6369e1
vzhovnitsky <vzhovnitsky@gmail.com>
2025-01-23 03:14:24 +0100

    fix: typo

--- app/utils/holders/transactionTypeFormatter.ts
+++ app/utils/holders/transactionTypeFormatter.ts
@@ -5,16 +5,16 @@ export function transactionTypeFormatter(
   isPending?: boolean
 ) {
   if (type === 'crypto_account_ready') {
-    return 'products.holders.transaction..type.accountReady';
+    return 'products.holders.transaction.type.accountReady';
   }
   if (type === 'card_ready') {
-    return 'products.holders.transaction..type.cardReady';
+    return 'products.holders.transaction.type.cardReady';
   }
   if (type === 'deposit') {
-    return 'products.holders.transaction..type.deposit';
+    return 'products.holders.transaction.type.deposit';
   }
   if (type === 'prepaid_topup') {
-    return 'products.holders.transaction..type.prepaidTopUp';
+    return 'products.holders.transaction.type.prepaidTopUp';
   }
   if (
     type === 'charge' ||
@@ -22,41 +22,41 @@ export function transactionTypeFormatter(
     type === 'prepaid_charge' ||
     type === 'charge_failed'
   ) {
-    return 'products.holders.transaction..type.payment';
+    return 'products.holders.transaction.type.payment';
   }
 
   if (type === 'decline') {
-    return 'products.holders.transaction..type.decline';
+    return 'products.holders.transaction.type.decline';
   }
 
   if (type === 'charge_back') {
-    return 'products.holders.transaction..type.refund';
+    return 'products.holders.transaction.type.refund';
   }
 
   if (type === 'limits_change') {
     if (isPending) {
-      return 'products.holders.transaction..type.limitsChanging';
+      return 'products.holders.transaction.type.limitsChanging';
     }
-    return 'products.holders.transaction..type.limitsChanged';
+    return 'products.holders.transaction.type.limitsChanged';
   }
   if (type === 'crypto_account_withdraw' || type === 'card_withdraw') {
-    return 'products.holders.transaction..type.cardWithdraw';
+    return 'products.holders.transaction.type.cardWithdraw';
   }
   if (type === 'contract_closed') {
-    return 'products.holders.transaction..type.contractClosed';
+    return 'products.holders.transaction.type.contractClosed';
   }
   if (type === 'card_block') {
-    return 'products.holders.transaction..type.cardBlock';
+    return 'products.holders.transaction.type.cardBlock';
   }
   if (type === 'card_freeze') {
-    return 'products.holders.transaction..type.cardFreeze';
+    return 'products.holders.transaction.type.cardFreeze';
   }
   if (type === 'card_unfreeze') {
-    return 'products.holders.transaction..type.cardUnfreeze';
+    return 'products.holders.transaction.type.cardUnfreeze';
   }
   if (type === 'card_paid') {
-    return 'products.holders.transaction..type.cardPaid';
+    return 'products.holders.transaction.type.cardPaid';
   }
 
-  return 'products.holders.transaction..type.unknown';
+  return 'products.holders.transaction.type.unknown';
 }

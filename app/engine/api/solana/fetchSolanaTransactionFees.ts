703a1dfad9df3d28551f48b1d22931f7d90df9cf
vzhovnitsky <vzhovnitsky@gmail.com>
2025-03-25 04:38:00 +0100

    wip: adding solana transfer fees

--- app/engine/api/solana/fetchSolanaTransactionFees.ts
+++ app/engine/api/solana/fetchSolanaTransactionFees.ts
@@ -7,8 +7,6 @@ const feesCodec = z.object({ value: z.number() });
 export async function fetchSolanaTransactionFees(transaction: string, isTestnet: boolean) {
     const network = isTestnet ? 'devnet' : 'mainnet';
     const url = `${whalesConnectEndpoint}/solana/transaction/fees/${network}`;
-    console.log('fetchSolanaTransactionFees', url);
     const res = await axios.post(url, { transaction });
-    console.log('fetchSolanaTransactionFees', res.data);
     return feesCodec.parse(res.data);
 }

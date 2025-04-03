e3a7b281fa2416504c012f998eee7e0324e90107
romust <schoolarts9ru@gmail.com>
2025-04-03 10:38:17 +0300

    fix: cr fixes

--- app/utils/ledger/checkLedgerTonAppVersion.ts
+++ app/utils/ledger/checkLedgerTonAppVersion.ts
@@ -1,13 +1,15 @@
 import { TonTransport } from "@ton-community/ton-ledger";
 
+const SUPPORTED_VERSION = '2.4.1';
+
 export const checkLedgerTonAppVersion = async (tonTransport?: TonTransport | null): Promise<boolean> => {
     if (!tonTransport) {
         return false;
     }
 
     try {
-        await tonTransport.getVersion();
-        return true;
+        const version = await tonTransport.getVersion();
+        return version >= SUPPORTED_VERSION;
     } catch {
         return false;
     }

2e9b8a9e41c2a0fa386d56426598a7688bae54dd
vzhovnitsky <vzhovnitsky@gmail.com>
2025-04-16 03:22:48 +0200

    wip: adding iOS target 'WonderPushNotificationServiceExtension'

---
+++ ios/WonderPushNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  WonderPushNotificationServiceExtension
+//
+//  Created by VZ on 16/4/25.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end

7832ddcf738aafed7092bc63fe678903a223c3f1
vzhovnitsky <vzhovnitsky@gmail.com>
2025-04-21 20:11:38 +0200

    fix: error sanitising

--- app/fragments/secure/utils/sanitizeErrorData.ts
+++ app/fragments/secure/utils/sanitizeErrorData.ts
@@ -1,16 +1,15 @@
 export function sanitizeErrorData(input: string): string {
     if (!input) return input;
 
-    // clear hex strings with separators from 20 characters onwards
-    // with 0x prefix
-    let sanitized = input.replace(/0x[0-9A-Fa-f][ -:]{1,2}[0-9A-Fa-f]{20,}/g, "[hex_string_data]");
-    // without 0x prefix
-    sanitized = sanitized.replace(/[0-9A-Fa-f][ -:]{1,2}[0-9A-Fa-f]{20,}/g, "[hex_string_data]");
-    // base58 strings from 20 characters onwards
-    sanitized = sanitized.replace(/[1-9A-HJ-NP-Za-km-z]{20,}/g, "[base58_string_data]");
-    // Uint8Array objects
-    if (sanitized.length > 0 && /^[\d,]+$/.test(sanitized) && sanitized.includes(',')) {
-        return "[uint8array_object]";
-    }
+    let sanitized = input
+        // hex, separators, 20+ chars
+        .replace(/0x[0-9A-Fa-f]{20,}/g, "[hex_string_data]")
+        // base58, 20+ chars
+        .replace(/(?:[0-9A-Fa-f]{2}(?:(?::| |-)[0-9A-Fa-f]{2}){9,}|[0-9A-Fa-f]{20,})/g, "[hex_string_data]")
+        // base58, 20+ chars
+        .replace(/[1-9A-HJ-NP-Za-km-z]{20,}/g, "[base58_string_data]")
+        // uint8array, 20+ chars
+        .replace(/\d{1,3}(?:,\d{1,3}){19,}/g, "[uint8array_object]");
+
     return sanitized;
 }

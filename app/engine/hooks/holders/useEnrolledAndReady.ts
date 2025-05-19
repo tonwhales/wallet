15ab4d623df30e1b1d5f66211d96d048d0913be6
romust <schoolarts9ru@gmail.com>
2025-05-19 15:02:55 +0300

    fix: ledger holders history and settings

--- app/engine/hooks/holders/useEnrolledAndReady.ts
+++ app/engine/hooks/holders/useEnrolledAndReady.ts
@@ -1,15 +1,19 @@
-import { useHoldersAccountStatus, useIsConnectAppReady, useNetwork, useSelectedAccount } from "..";
+import { useHoldersAccountStatus, useIsConnectAppReady, useIsLedgerRoute, useNetwork, useSelectedAccount } from "..";
 import { HoldersUserState } from "../../api/holders/fetchUserState";
 import { holdersUrl as resolveHoldersUrl } from "../../api/holders/fetchUserState";
+import { useLedgerTransport } from "../../../fragments/ledger/components/TransportContext";
+import { Address } from "@ton/ton";
 
-export function useEnrolledAndReady(address?: string) {
+export function useEnrolledAndReady() {
     const { isTestnet } = useNetwork();
+    const isLedger = useIsLedgerRoute();
     const selected = useSelectedAccount();
+    const ledgerContext = useLedgerTransport();
     const holdersUrl = resolveHoldersUrl(isTestnet);
-    const _address = address ?? selected!.address.toString({ testOnly: isTestnet });
-    const status = useHoldersAccountStatus(_address).data;
-    const isTonconnectReady = useIsConnectAppReady(holdersUrl, _address);
-
+    const address = isLedger ? Address.parse(ledgerContext.addr!.address) : selected!.address;
+    const addressString = address.toString({ testOnly: isTestnet }); 
+    const status = useHoldersAccountStatus(address).data;
+    const isTonconnectReady = useIsConnectAppReady(holdersUrl, addressString);
     const enrolled = status?.state !== HoldersUserState.NeedEnrollment
         && status?.state !== HoldersUserState.NoRef;
 

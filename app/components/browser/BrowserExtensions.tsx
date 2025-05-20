e5f32e4ef26491d18066e927fa956517d31d30ae
vzhovnitsky <vzhovnitsky@gmail.com>
2025-05-20 15:08:04 +0200

    fix: tc disconnect event

--- app/components/browser/BrowserExtensions.tsx
+++ app/components/browser/BrowserExtensions.tsx
@@ -26,149 +26,142 @@ export const EmptyIllustrations = {
   light: require('@assets/empty-connections.webp'),
 };
 
-export const BrowserExtensions = memo(
-  ({ onScroll }: { onScroll?: (event: NativeSyntheticEvent<NativeScrollEvent>) => void }) => {
-    const theme = useTheme();
-    const safeArea = useSafeAreaInsets();
-    const { isTestnet } = useNetwork();
-    const bottomBarHeight = useBottomTabBarHeight();
-    const dimensions = useDimensions();
-    const navigation = useTypedNavigation();
-    const [installedExtensions] = useExtensions();
-    const [inastalledConnectApps] = useTonConnectExtensions();
-    const holdersUrl = resolveHoldersUrl(isTestnet);
+export const BrowserExtensions = memo(({ onScroll }: { onScroll?: (event: NativeSyntheticEvent<NativeScrollEvent>) => void }) => {
+  const theme = useTheme();
+  const safeArea = useSafeAreaInsets();
+  const { isTestnet } = useNetwork();
+  const bottomBarHeight = useBottomTabBarHeight();
+  const dimensions = useDimensions();
+  const navigation = useTypedNavigation();
+  const [installedExtensions] = useExtensions();
+  const [inastalledConnectApps] = useTonConnectExtensions();
+  const holdersUrl = resolveHoldersUrl(isTestnet);
 
-    const extensions = Object.entries(installedExtensions.installed).map(([key, ext]) => {
-      const appData = getCachedAppData(ext.url);
-      return { ...ext, key, title: appData?.title || ext.title || ext.url };
+  const extensions = Object.entries(installedExtensions.installed).map(([key, ext]) => {
+    const appData = getCachedAppData(ext.url);
+    return { ...ext, key, title: appData?.title || ext.title || ext.url };
+  });
+
+  const tonconnectApps = Object.entries(inastalledConnectApps)
+    .map(([key, ext]) => ({ ...ext, key }))
+    .filter((v) => v.url !== holdersUrl);
+
+  const removeExtension = useRemoveExtension();
+  const disconnectConnect = useDisconnectApp();
+
+  const openExtension = useCallback((url: string) => {
+    let domain = extractDomain(url);
+    if (!domain) {
+      return; // Shouldn't happen
+    }
+    let k = getDomainKey(domain);
+    if (!k) {
+      navigation.navigate('Install', { url });
+    } else {
+      navigation.navigate('App', { url });
+    }
+  }, []);
+
+  const openTonconnectApp = useCallback((item: ConnectedApp) => {
+    const domain = extractDomain(item.url);
+
+    navigation.navigateDAppWebView({
+      lockNativeBack: true,
+      safeMode: true,
+      url: item.url,
+      title: item.name ?? undefined,
+      header: { title: { type: 'params', params: { title: item.name, domain } } },
+      useStatusBar: true,
+      engine: 'ton-connect',
+      controlls: {
+        refresh: true,
+        share: true,
+        back: true,
+        forward: true,
+      },
     });
+  }, []);
 
-    const tonconnectApps = Object.entries(inastalledConnectApps)
-      .map(([key, ext]) => ({ ...ext, key }))
-      .filter((v) => v.url !== holdersUrl);
-
-    const removeExtension = useRemoveExtension();
-    const disconnectConnect = useDisconnectApp();
-
-    const openExtension = useCallback((url: string) => {
-      let domain = extractDomain(url);
-      if (!domain) {
-        return; // Shouldn't happen
-      }
-      let k = getDomainKey(domain);
-      if (!k) {
-        navigation.navigate('Install', { url });
-      } else {
-        navigation.navigate('App', { url });
-      }
-    }, []);
-
-    const openTonconnectApp = useCallback((item: ConnectedApp) => {
-      const domain = extractDomain(item.url);
-
-      navigation.navigateDAppWebView({
-        lockNativeBack: true,
-        safeMode: true,
-        url: item.url,
-        title: item.name ?? undefined,
-        header: { title: { type: 'params', params: { title: item.name, domain } } },
-        useStatusBar: true,
-        engine: 'ton-connect',
-        controlls: {
-          refresh: true,
-          share: true,
-          back: true,
-          forward: true,
+  const onRemoveExtension = useCallback((key: string) => {
+    Alert.alert(t('auth.revoke.title'), t('auth.revoke.message'), [
+      { text: t('common.cancel') },
+      {
+        text: t('auth.revoke.action'),
+        style: 'destructive',
+        onPress: () => {
+          removeExtension(key);
         },
-      });
-    }, []);
-
-    const onRemoveExtension = useCallback(
-      (key: string) => {
-        Alert.alert(t('auth.revoke.title'), t('auth.revoke.message'), [
-          { text: t('common.cancel') },
-          {
-            text: t('auth.revoke.action'),
-            style: 'destructive',
-            onPress: () => {
-              removeExtension(key);
-            },
-          },
-        ]);
       },
-      [removeExtension],
-    );
+    ]);
+  }, [removeExtension]);
 
-    let disconnectConnectApp = useCallback(
-      (key: string) => {
-        Alert.alert(t('auth.revoke.title'), t('auth.revoke.message'), [
-          { text: t('common.cancel') },
-          {
-            text: t('auth.revoke.action'),
-            style: 'destructive',
-            onPress: () => {
-              disconnectConnect(key, 0);
-            },
-          },
-        ]);
+  let disconnectConnectApp = useCallback((key: string) => {
+    Alert.alert(t('auth.revoke.title'), t('auth.revoke.message'), [
+      { text: t('common.cancel') },
+      {
+        text: t('auth.revoke.action'),
+        style: 'destructive',
+        onPress: () => {
+          disconnectConnect(key, 0);
+        },
       },
-      [disconnectConnect],
-    );
+    ]);
+  }, [disconnectConnect]);
 
-    return extensions.length === 0 && tonconnectApps.length === 0 ? (
-      <View>
-        <Image
-          resizeMode={'center'}
-          style={{
-            height: dimensions.screen.width - 32,
-            width: dimensions.screen.width - 32,
-            marginTop: -20,
-          }}
-          source={EmptyIllustrations[theme.style]}
-        />
-        <Text
-          style={{
-            fontSize: 32,
-            fontWeight: '600',
-            marginHorizontal: 24,
-            textAlign: 'center',
-            color: theme.textPrimary,
-          }}>
-          {t('auth.noExtensions')}
-        </Text>
-      </View>
-    ) : (
-      <Animated.ScrollView
-        entering={FadeIn}
-        exiting={FadeOut}
-        style={{ flexGrow: 1 }}
-        onScroll={onScroll}
-        scrollEventThrottle={16}
-        contentContainerStyle={{ justifyContent: 'center', alignItems: 'center', gap: 8 }}>
-        {extensions.map((app) => (
-          <View key={`ton-x-app-${app.url}`} style={{ width: '100%' }}>
-            <ConnectionButton
-              onPress={() => openExtension(app.url)}
-              onRevoke={() => onRemoveExtension(app.url)}
-              onLongPress={() => onRemoveExtension(app.url)}
-              url={app.url}
-              name={app.title}
-            />
-          </View>
-        ))}
-        {tonconnectApps.map((app) => (
-          <View key={`connect-app-${app.url}`} style={{ width: '100%' }}>
-            <ConnectionButton
-              onRevoke={() => disconnectConnectApp(app.url)}
-              onPress={() => openTonconnectApp(app)}
-              url={app.url}
-              name={app.name}
-              tonconnect
-            />
-          </View>
-        ))}
-        <View style={{ height: bottomBarHeight + safeArea.top + safeArea.bottom + 256 }} />
-      </Animated.ScrollView>
-    );
-  },
+  return extensions.length === 0 && tonconnectApps.length === 0 ? (
+    <View>
+      <Image
+        resizeMode={'center'}
+        style={{
+          height: dimensions.screen.width - 32,
+          width: dimensions.screen.width - 32,
+          marginTop: -20,
+        }}
+        source={EmptyIllustrations[theme.style]}
+      />
+      <Text
+        style={{
+          fontSize: 32,
+          fontWeight: '600',
+          marginHorizontal: 24,
+          textAlign: 'center',
+          color: theme.textPrimary,
+        }}>
+        {t('auth.noExtensions')}
+      </Text>
+    </View>
+  ) : (
+    <Animated.ScrollView
+      entering={FadeIn}
+      exiting={FadeOut}
+      style={{ flexGrow: 1 }}
+      onScroll={onScroll}
+      scrollEventThrottle={16}
+      contentContainerStyle={{ justifyContent: 'center', alignItems: 'center', gap: 8 }}>
+      {extensions.map((app) => (
+        <View key={`ton-x-app-${app.url}`} style={{ width: '100%' }}>
+          <ConnectionButton
+            onPress={() => openExtension(app.url)}
+            onRevoke={() => onRemoveExtension(app.url)}
+            onLongPress={() => onRemoveExtension(app.url)}
+            url={app.url}
+            name={app.title}
+          />
+        </View>
+      ))}
+      {tonconnectApps.map((app) => (
+        <View key={`connect-app-${app.url}`} style={{ width: '100%' }}>
+          <ConnectionButton
+            onRevoke={() => disconnectConnectApp(app.url)}
+            onPress={() => openTonconnectApp(app)}
+            url={app.url}
+            name={app.name}
+            tonconnect
+          />
+        </View>
+      ))}
+      <View style={{ height: bottomBarHeight + safeArea.top + safeArea.bottom + 256 }} />
+    </Animated.ScrollView>
+  );
+},
 );
--- app/engine/hooks/dapps/useDisconnect.ts
+++ app/engine/hooks/dapps/useDisconnect.ts
@@ -1,4 +1,4 @@
-import { SessionCrypto, WalletResponse } from "@tonconnect/protocol";
+import { DisconnectEvent, SessionCrypto, WalletResponse } from "@tonconnect/protocol";
 import { useConnectApp } from "../../hooks/dapps/useConnectApp";
 import { useAppConnections } from "../../hooks/dapps/useAppConnections";
 import { extensionKey } from "./useAddExtension";
@@ -15,6 +15,13 @@ export function useDisconnectApp(address?: string) {
 
     return (endpoint: string, eventId: number | string) => {
         const app = getConnectApp(endpoint);
+        let id = 0;
+
+        if (typeof eventId === 'number') {
+            id = eventId;
+        } else {
+            id = parseInt(eventId);
+        }
 
         if (!app) {
             return;
@@ -28,7 +35,7 @@ export function useDisconnectApp(address?: string) {
         remoteConnections.forEach((connection) => {
             // Send disconnect event
             const sessionCrypto = new SessionCrypto(connection.sessionKeyPair);
-            const event: WalletResponse<'disconnect'> = { result: {}, id: eventId.toString() };
+            const event: DisconnectEvent = { event: 'disconnect', id, payload: {} };
             sendTonConnectResponse({ response: event, sessionCrypto, clientSessionId: connection.clientSessionId });
         });
 

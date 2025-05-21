daa04703f61ca006f7e19b0e1dabc49405c2cb65
romust <schoolarts9ru@gmail.com>
2025-05-21 14:14:43 +0300

    fix: duplicated dapps in browser search

--- app/engine/hooks/dapps/useDAppsSuggestions.ts
+++ app/engine/hooks/dapps/useDAppsSuggestions.ts
@@ -17,39 +17,39 @@ const knownDApps = {
         title: 'Tonhub',
         icon: 'https://ucarecdn.com/7d75de5c-da0c-43cf-8f34-3cb7533dd481/'
     },
-    'https://dedust.io/': {
+    'https://dedust.io': {
         title: 'DeDust.io',
         icon: 'https://dedust.io/apple-touch-icon.png'
     },
-    'https://tonwhales.com/': {
+    'https://tonwhales.com': {
         title: 'Ton Whales',
         icon: 'https://ucarecdn.com/b934939e-31a1-43da-b996-c3f3e041d8d6/'
     },
-    'https://tonviewer.com/': {
+    'https://tonviewer.com': {
         title: 'TON Explorer by Tonviewer: Blockchain Analysis',
         icon: 'https://ucarecdn.com/2204e9b4-f577-47d6-9581-5882c140b2bc/'
     },
-    'https://getgems.io/': {
+    'https://getgems.io': {
         title: 'Getgems — TON NFT Marketplace',
         icon: 'https://ucarecdn.com/39b288eb-9f38-439e-8b00-89b816a5387b/'
     },
-    'https://gmt.io/ton/': {
+    'https://gmt.io/ton': {
         title: 'GoMining Whales NFT',
         icon: 'https://ucarecdn.com/0a977ee2-8143-48b5-90e3-8d7925970e15/'
     },
-    'https://ston.fi/': {
+    'https://ston.fi': {
         title: 'STON.fi — AMM DEX for the TON blockchain',
         icon: 'https://ucarecdn.com/4ab891ea-5728-4612-9fc0-42bcb8d19ca6/'
     },
-    'https://holders.io/': {
+    'https://holders.io': {
         title: 'Holders',
         icon: 'https://ucarecdn.com/490986ac-c285-4932-a67f-c75c38f34283/'
     },
-    'https://fragment.com/': {
+    'https://fragment.com': {
         title: 'Fragment',
         icon: 'https://ucarecdn.com/b6b4100e-3194-4f9b-9210-c924a9f92988/'
     },
-    'https://dns.ton.org/': {
+    'https://dns.ton.org': {
         title: 'Buy .ton domains',
         icon: 'https://ucarecdn.com/7a3bdafd-13d1-484a-89c0-87de5836c9a2/'
     },

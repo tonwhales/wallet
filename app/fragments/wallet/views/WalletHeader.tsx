c2ac55175c455548dddb13cc650050b3af1fea1b
romust <schoolarts9ru@gmail.com>
2025-10-22 16:40:56 +0700

    fix: rate icon

--- app/fragments/wallet/views/WalletHeader.tsx
+++ app/fragments/wallet/views/WalletHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { memo, useCallback } from "react";
 import { Pressable, View, Platform, ScrollView, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -15,7 +15,7 @@ import { useRates } from "../../../engine/hooks/currency/useRates";
 import { t } from "../../../i18n/t";
 import { IcSupportMain } from "@assets";
 
-import IcRateChevron from '@assets/ic_rate_chevron.svg';
+import ArrowIcon from '@assets/order/arrow-without-background.svg';
 
 const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
 
@@ -27,10 +27,8 @@ export const WalletHeader = memo(({ address, height, walletCardHeight, scrollOff
     const rates = useRates(['ton'], ['usd'])?.rates;
     const diff = rates?.TON?.diff24h?.USD;
     const isNegative = diff?.startsWith('−');
-    const diffPercent = diff?.replace(/^[+−]/, '');
     const { onHelpCenter, notifications } = useSupport();
     const diffTextColor = isNegative ? theme.accentRed : theme.accentGreen;
-    const diffBackgroundColor = isNegative ? theme.accentRed + '30' : theme.accentGreen + '30';
 
     const animatedStyle = useAnimatedStyle(() => ({
         transform: [
@@ -52,9 +50,21 @@ export const WalletHeader = memo(({ address, height, walletCardHeight, scrollOff
         const showToggle = scrollOffsetSv.value > 0;
         return {
             opacity: withTiming(showToggle ? 0 : 1, { duration: 200 }),
-            flexDirection: 'row'
+            flexDirection: 'row',
+            alignItems: 'center'
         };
     });
+
+    const supportIcon = useMemo(() => {
+        return (
+            <View style={{ height: 36, width: 36, borderRadius: 18, alignItems: 'center', justifyContent: 'center' }}>
+                <IcSupportMain width={34} height={34} />
+                {((notifications ?? 0) > 0) && (
+                    <View style={{ position: 'absolute', top: 2, right: 2, backgroundColor: theme.accentRed, borderRadius: 10, width: 10, height: 10 }} />
+                )}
+            </View>
+        )
+    }, [])
 
     return (
         <Animated.View
@@ -102,7 +112,7 @@ export const WalletHeader = memo(({ address, height, walletCardHeight, scrollOff
                                 <PriceComponent
                                     showSign
                                     amount={toNano(1)}
-                                    style={{ backgroundColor: 'transparent', paddingHorizontal: 6 }}
+                                    style={{ backgroundColor: 'transparent', paddingLeft: 6, paddingRight: 0 }}
                                     textStyle={{ color: theme.style === 'light' ? theme.textOnsurfaceOnDark : theme.textPrimary }}
                                     theme={theme}
                                 />
@@ -110,20 +120,26 @@ export const WalletHeader = memo(({ address, height, walletCardHeight, scrollOff
                             </Pressable>
                             {!!diff && (
                                 <View
-                                    style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: diffBackgroundColor, paddingHorizontal: 6, borderRadius: 20 }}
+                                    style={{ flexDirection: 'row', alignItems: 'center' }}
                                 >
-                                    <IcRateChevron
-                                        width={12}
-                                        height={12}
+                                    <ArrowIcon
+                                        width={15}
+                                        height={15}
                                         color={diffTextColor}
                                         style={{
                                             transform: [{ rotate: isNegative ? '180deg' : '0deg' }],
                                             marginRight: 4
                                         }}
                                     />
-                                    <Text style={[Typography.medium15_20, { color: diffTextColor }]}>
-                                        {diffPercent}
-                                    </Text>
+                                    <Pressable
+                                        style={({ pressed }) => [
+                                            { flexDirection: 'row', alignItems: 'center', gap: 8 },
+                                            { opacity: pressed ? 0.8 : 1 }
+                                        ]}
+                                        onPress={onHelpCenter}
+                                    >
+                                        {supportIcon}
+                                    </Pressable>
                                 </View>
                             )}
                         </Animated.View>
@@ -147,12 +163,7 @@ export const WalletHeader = memo(({ address, height, walletCardHeight, scrollOff
                                     <Text style={[{ color: theme.textUnchangeable }, Typography.medium15_20]}>
                                         {t('settings.support.title')}
                                     </Text>
-                                    <View style={{ height: 36, width: 36, borderRadius: 18, alignItems: 'center', justifyContent: 'center' }}>
-                                        <IcSupportMain width={34} height={34} />
-                                        {((notifications ?? 0) > 0) && (
-                                            <View style={{ position: 'absolute', top: 2, right: 2, backgroundColor: theme.accentRed, borderRadius: 10, width: 10, height: 10 }} />
-                                        )}
-                                    </View>
+                                    {supportIcon}
                                 </Pressable>
                             </View>
                         </Animated.View>

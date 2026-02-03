0221e3584bbad30121149dd0fb0208b9cc7210cb
vzhovnitsky <vzhovnitsky@gmail.com>
2026-02-04 00:31:50 +0100

    fix: rm perf text, todo: refactor imports

--- app/components/basic/PerfText.tsx
+++ app/components/basic/PerfText.tsx
@@ -1,5 +1,1 @@
-import { NativeMethods, TextComponent } from 'react-native'
-
-type Constructor<T> = new (...args: any[]) => T
-
-export const PerfText: Constructor<NativeMethods> & typeof TextComponent = require('react-native/Libraries/Text/TextNativeComponent').NativeText
+export { Text as PerfText } from 'react-native';
